e805de39dfc80382d54e9b343f7155d674dc8b1e
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-13 12:04:34 +0530

    Time: 172 ms (38.76%), Space: 25.5 MB (60.49%) - LeetHub

---
+++ 87-scramble-string/87-scramble-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    unordered_map<string,unordered_map<string,bool>> mp;
+    
+    bool find(string s,string t){
+
+        if(mp.count(s) && mp[s].count(t)){
+            return mp[s][t];
+        }
+        
+        if(s == t)return mp[s][t] = true;
+        
+        int n = s.size();
+        
+        if(n == 1)return mp[s][t] = false;
+        
+        for(int k=1;k<n;k++){
+
+           bool f1 = find(s.substr(0,k),t.substr(0,k)) && find(s.substr(k),t.substr(k));
+           bool f2 = find(s.substr(0,k), t.substr(n-k)) && find(s.substr(k),t.substr(0,n-k));
+            
+          if(f1 || f2)return mp[s][t] = true;
+        }
+        
+        return mp[s][t] = false;
+    }
+    
+    
+    bool isScramble(string s, string t) {
+        
+        int n = s.size();
+        return find(s,t);
+        
+    }
+};
