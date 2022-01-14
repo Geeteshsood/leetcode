c4f39df042e5363d003d159a0f2ea859b1e38742
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-15 00:11:22 +0530

    Time: 1220 ms (5.06%), Space: 29.6 MB (38.00%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
-    
-    int find(int e,int f,vector<vector<int>> &dp){
+    int superEggDrop(int k, int n) {
         
-       if(dp[e][f] != -1){
-           return dp[e][f];
-       }
+        vector<vector<int>> dp(k+1,vector<int>(n+1));
         
-       if(f == 1)return dp[e][f] = 1;
-       else if(f == 0)return dp[e][f] = 0;
-       else if(e == 1)return dp[e][f] = f;
-        
-       int ans = INT_MAX , val = 0;
-        
-       int start = 1 , end = f;
-        
-        while(start<=end){
+        for(int f=1;f<n+1;f++)dp[1][f] = f;
             
-            int mid = end - (end - start)/2;
-            
-           int down = 1 + find(e-1,mid-1,dp);
-           int up = 1 + find(e,f-mid,dp);
-         
-           if(down < up){
-               start = mid + 1;
-           }
-           else{
-               end = mid - 1;
-           }
-            
-           val = max(down,up);
-           ans = min(ans,val);
-       }
+        for(int e=2;e<k+1;e++){
+            for(int f=1;f<n+1;f++){
+                int ans = INT_MAX ,val = 0;
+                
+                int start = 1 , end = f;
+                
+                while(start <= end){
+                     int mid = end - (end-start)/2;
+                     int up = dp[e][f-mid] , down = dp[e-1][mid-1];
+                    
+                     if(up > down){
+                         start = mid + 1;
+                     }
+                     else end = mid - 1;
+                    
+                     val = 1 + max(down , up);
+                     ans = min(val,ans);
+                }
+                dp[e][f] = ans;
+            }
+        }
         
-       return dp[e][f] = ans;
-    }
-    
-    int superEggDrop(int e, int n) {
-        
-        vector<vector<int>> dp(e+1,vector<int>(n+1,-1));
-        
-        return find(e,n,dp);
+     return dp[k][n];
     }
 };
