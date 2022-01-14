1925c324d5564be967bcb533f244e5c09b1320e8
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-14 16:02:17 +0530

    Added solution - LeetHub

--- Largest Sum Subarray of Size at least K - GFG/largest-sum-subarray-of-size-at-least-k.cpp
+++ Largest Sum Subarray of Size at least K - GFG/largest-sum-subarray-of-size-at-least-k.cpp
@@ -25,29 +25,12 @@ int main() {
 // } Driver Code Ends
 
 /*
-k = 3.
-
- 0  1  2  3  4  5  
- ----------------
-    -------------
-       ----------
-          -------
-    
- 0  1  2  3  4  5  
- -------------
-    ----------
-       -------
-        
-agar hum 4 se uska best mangege  
-uske paas >=3 size ka max subarray ka sum pada hoga jo 4 par end ho rahe hai.
-    
-us maximum me agar hum 5 add karde.
-toh humare paas >=4 size ka max subarray ka sum aa jayega jo 5 par end ho rahe hai.
-    
-isko hum 5 par end hone wale 3 size ke subarray ke sum ke saath compare kar lenge.
-    
-usse hum 5 par khatam hone wale >=3 size (or >=k ) subarray ka max sum mil jayenga.
-
+ this is 2 nd approach pepcoding channel
+  
+ pepcoding.
+ 
+ 
+ 
 */
 
 long long int maxSumWithK(long long int arr[], long long int n, long long int k) 
