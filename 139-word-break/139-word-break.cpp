ae07712523c5ef1a7df34562274f3d1e00fb2411
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-14 13:34:14 +0530

    Time: 36 ms (15.89%), Space: 14.7 MB (18.32%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
     
-    unordered_set<string> se;
-    
-    bool find(int i,int j,string &s,vector<int> &dp){
-        
-         if(dp[i] != -1)return dp[i];
-        
-         if(se.count(s.substr(i,j-i+1)))return dp[i] = true;
-        
-         if(i == j)return dp[i] = false;
-        
-        for(int k=i;k<j;k++){
-            
-            string str = s.substr(i,k-i+1);
-            if(se.count(str)){
-                if(find(k+1,j,s,dp)){
-                    return dp[i] = true;
-                }
-            }
-            
-        }
-        
-      return dp[i] = false;  
-    }
-    
-    
     bool wordBreak(string s, vector<string>& wordDict) {
+        
+        unordered_set<string> se;
         
         for(auto &i:wordDict){
             se.insert(i);
         }
         
         int n = s.size();
-        vector<int> dp(n,-1);
+        vector<bool> dp(n);
         
-        return find(0,n-1,s,dp);
+        for(int j=0;j<n;j++){
+            for(int i=0;i<=j;i++){
+                string str = s.substr(i,j-i+1);
+                if(se.count(str) && (i == 0 || dp[i-1])){
+                    dp[j] = true;
+                    break;
+                }
+            }
+        }
+        
+     return dp[n-1] ;  
     }
 };
