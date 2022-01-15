e02466860b3e3c606e2b500f8408ed9ec1f7e93a
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-15 16:54:22 +0530

    Time: 2 ms (69.40%), Space: 7.8 MB (29.80%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,42 +1,47 @@
+typedef pair<int,int> pi;
 class Solution {
 public:
     
-    pair<int,int> find(int i,int j,vector<int> &nums,bool flag){
+    pi find(int i,int j,vector<int> &nums,bool flag,vector<vector<pi>> &dp){
+        
+        if(dp[i][j].first != -1){
+            return dp[i][j];
+        }
         
         if(i == j){
-            if(flag)return {nums[i],0};
-            else return {0,nums[j]};
+            if(flag)return dp[i][j] = {nums[i],0};
+            else return dp[i][j] = {0,nums[j]};
         }
         
         if(flag){ 
-            auto[fp1,sp1] = find(i+1,j,nums,false);
-            auto[fp2,sp2] = find(i,j-1,nums,false);
+            auto[fp1,sp1] = find(i+1,j,nums,false,dp);
+            auto[fp2,sp2] = find(i,j-1,nums,false,dp);
             
             fp1 += nums[i] , fp2 += nums[j];
             
-            if(fp1 > fp2)return {fp1,sp1};
-            else return {fp2,sp2};
+            if(fp1 > fp2)return dp[i][j] = {fp1,sp1};
+            else return dp[i][j] = {fp2,sp2};
         }
         else{
             
-            auto[fp1,sp1] = find(i+1,j,nums,true);
-            auto[fp2,sp2] = find(i,j-1,nums,true);
+            auto[fp1,sp1] = find(i+1,j,nums,true,dp);
+            auto[fp2,sp2] = find(i,j-1,nums,true,dp);
             
             sp1 += nums[i] , sp2 += nums[j];
             
-            if(sp1 > sp2)return {fp1,sp1};
-            else return {fp2,sp2};
+            if(sp1 > sp2)return dp[i][j] = {fp1,sp1};
+            else return dp[i][j] = {fp2,sp2};
         }
      
-     return {-1,-1};
+     return dp[i][j] = {-1,-1};
     }
     
     
     bool PredictTheWinner(vector<int>& nums) {
         
         int n = nums.size();
-        
-        auto [fp,sp] = find(0,n-1,nums,true);
+        vector<vector<pi>> dp(n,vector<pi>(n,{-1,-1}));
+        auto [fp,sp] = find(0,n-1,nums,true,dp);
         
       return fp>=sp;  
     }
