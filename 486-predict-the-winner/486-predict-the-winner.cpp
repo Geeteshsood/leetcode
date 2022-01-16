b32f6793a9904c8b712643a589d622b67fc3fdf8
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-16 14:58:52 +0530

    Time: 276 ms (7.45%), Space: 7.4 MB (69.02%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    bool PredictTheWinner(vector<int>& nums) {
-        
-        int n = nums.size() , total = 0;
-        vector<vector<int>> dp(n,vector<int>(n));
-        
-         for(int i=0;i<n;i++){
-             total+=nums[i];
-            for(int j=i;j<n;j++){
-               if(j>0)dp[i][j] = nums[j] + dp[i][j-1];
-               else dp[0][0] = nums[0];
-            }
-        }
-        
-        for(int g=1;g<n;g++){
-            for(int i=0,j=g;i<n && j<n;i++,j++){
-                dp[i][j] = max(dp[i][j] - dp[i][j-1],dp[i][j] - dp[i+1][j]);
-            }
-        }
+    
+    int find(int i,int j,vector<int> &nums){
 
-        int fp = dp[0][n-1];
-        int sp = total-fp;
+        if(i == j)return nums[i];
+        else if(i > j)return 0;
+        
+        int x = nums[i] + min(find(i+2,j,nums),find(i+1,j-1,nums));
+        int y = nums[j] + min(find(i+1,j-1,nums),find(i,j-2,nums));
+        
+        return max(x,y);
+    }
+    
+    bool PredictTheWinner(vector<int>& nums){
+        
+       int n = nums.size() , total = 0;
+       for(int i=0;i<n;i++)total += nums[i];
+        
+       int fp = find(0,n-1,nums);
+       int sp = total - fp;
         
      return fp>=sp;
     }
