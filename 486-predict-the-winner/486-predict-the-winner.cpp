f9b202d18240a0c81e752f3820cfe612b8d7bec3
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-16 13:56:18 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (39.30%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -3,30 +3,25 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n)) ,sum(n,vector<int>(n));
+        vector<vector<int>> dp(n,vector<int>(n));
         
          for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-               if(j>0)sum[i][j] = nums[j] + sum[i][j-1];
-               else sum[0][0] = nums[0];
+               if(j>0)dp[i][j] = nums[j] + dp[i][j-1];
+               else dp[0][0] = nums[0];
             }
         }
         
-        for(int g=0;g<n;g++){
+        for(int g=1;g<n;g++){
             for(int i=0,j=g;i<n && j<n;i++,j++){
-                if(g == 0){
-                    dp[i][j] = nums[i];
-                    continue;
-                }
-                
-                int x = nums[j] + sum[i][j-1] - dp[i][j-1];
-                int y = nums[i] + sum[i+1][j] - dp[i+1][j];
-                
-                dp[i][j] = max(x,y);
+                dp[i][j] -= min(dp[i][j-1],dp[i+1][j]);
             }
         }
 
-        int total = sum[0][n-1],fp = dp[0][n-1];
+        int total = 0;
+        for(int i=0;i<n;i++)total+=nums[i];
+        
+        int fp = dp[0][n-1];
         int sp = total-fp;
         
      return fp>=sp;
