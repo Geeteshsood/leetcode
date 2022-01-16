2d5ddac5a4d06365b1a5259a9329148411a576ba
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-16 14:02:35 +0530

    Added solution - LeetHub

--- Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
+++ Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
@@ -10,31 +10,22 @@ class Solution{
     public:
     long long maximumAmount(int nums[], int n){
 
-        vector<vector<ll>> dp(n,vector<ll>(n)) ,sum(n,vector<ll>(n));
+        vector<vector<ll>> dp(n,vector<ll>(n));
         
          for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-               if(j>0)sum[i][j] = (ll)nums[j] + sum[i][j-1];
-               else sum[0][0] = (ll)nums[0];
+               if(j>0)dp[i][j] = (ll)nums[j] + dp[i][j-1];
+               else dp[0][0] = (ll)nums[0];
             }
         }
         
-        for(int g=0;g<n;g++){
+        for(int g=1;g<n;g++){
             for(int i=0,j=g;i<n && j<n;i++,j++){
-                if(g == 0){
-                    dp[i][j] = nums[i];
-                    continue;
-                }
-                
-                ll x = (ll)nums[j] + sum[i][j-1] - dp[i][j-1];
-                ll y = (ll)nums[i] + sum[i+1][j] - dp[i+1][j];
-                
-                dp[i][j] = max(x,y);
+                dp[i][j] = max(dp[i][j] - dp[i][j-1],dp[i][j] - dp[i+1][j]);
             }
-        }
-
+        };
+        
      return dp[0][n-1];
-       
     }
 };
 
