6639e997934c89cf3c496c2c8fc0dbfc4af3e006
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-16 15:00:23 +0530

    Time: 4 ms (63.87%), Space: 7.8 MB (29.56%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     
-    int find(int i,int j,vector<int> &nums){
+    int find(int i,int j,vector<int> &nums,vector<vector<int>> &dp){
 
         if(i == j)return nums[i];
         else if(i > j)return 0;
         
-        int x = nums[i] + min(find(i+2,j,nums),find(i+1,j-1,nums));
-        int y = nums[j] + min(find(i+1,j-1,nums),find(i,j-2,nums));
+        if(dp[i][j] != -1)return dp[i][j];
         
-        return max(x,y);
+        int x = nums[i] + min(find(i+2,j,nums,dp),find(i+1,j-1,nums,dp));
+        int y = nums[j] + min(find(i+1,j-1,nums,dp),find(i,j-2,nums,dp));
+        
+        return dp[i][j] = max(x,y);
     }
     
     bool PredictTheWinner(vector<int>& nums){
@@ -17,7 +19,9 @@ public:
        int n = nums.size() , total = 0;
        for(int i=0;i<n;i++)total += nums[i];
         
-       int fp = find(0,n-1,nums);
+       vector<vector<int>> dp(n,vector<int>(n,-1));
+        
+       int fp = find(0,n-1,nums,dp);
        int sp = total - fp;
         
      return fp>=sp;
