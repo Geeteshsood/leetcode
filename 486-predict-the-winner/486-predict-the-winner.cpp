4aef4367210d9f39634597f44982ff27f1899b4d
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-16 14:01:17 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (17.67%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     bool PredictTheWinner(vector<int>& nums) {
         
-        int n = nums.size();
+        int n = nums.size() , total = 0;
         vector<vector<int>> dp(n,vector<int>(n));
         
          for(int i=0;i<n;i++){
+             total+=nums[i];
             for(int j=i;j<n;j++){
                if(j>0)dp[i][j] = nums[j] + dp[i][j-1];
                else dp[0][0] = nums[0];
@@ -14,13 +15,10 @@ public:
         
         for(int g=1;g<n;g++){
             for(int i=0,j=g;i<n && j<n;i++,j++){
-                dp[i][j] -= min(dp[i][j-1],dp[i+1][j]);
+                dp[i][j] = max(dp[i][j] - dp[i][j-1],dp[i][j] - dp[i+1][j]);
             }
         }
 
-        int total = 0;
-        for(int i=0;i<n;i++)total+=nums[i];
-        
         int fp = dp[0][n-1];
         int sp = total-fp;
         
