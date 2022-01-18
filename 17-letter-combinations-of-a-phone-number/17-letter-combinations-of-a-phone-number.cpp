b2fe2c8bf7911e382dac6e82b8bddefa0465f171
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-18 12:55:09 +0530

    Time: 9 ms (5.07%), Space: 6.6 MB (36.58%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
     
-    unordered_map<char,string> mp;
-    
-    vector<string> find(int i,string &s){
-        
-        if(i == s.size())return {""};
-        
-        vector<string> ans;
-        
-        auto res =  find(i+1,s);
-      
-        string str = mp[s[i]-'0'];
-        
-        for(auto &it:str){
-            for(auto &x:res){
-                ans.push_back(it + x);
-            }
-        }
-        
-      return ans;  
-    }
-    
     vector<string> letterCombinations(string s) {
         
-        mp[2] = "abc" , mp[3] = "def" , mp[4] = "ghi" , mp[5] = "jkl" , mp[6] = "mno" , mp[7] = "pqrs" ,         mp[8] = "tuv" , mp[9] = "wxyz";
+      if(s.size() == 0)return {};
         
-        vector<string> res = find(0,s);
+      unordered_map<int,string> mp;
         
-        if(res[0] == "")return {};
-            
-     return res;
+      mp[2] = "abc" , mp[3] = "def" , mp[4] = "ghi" , mp[5] = "jkl" , mp[6] = "mno" , mp[7] = "pqrs" ,         mp[8] = "tuv" , mp[9] = "wxyz";
+        
+      int n = s.size();
+      vector<string> cur{{""}};
+        
+      for(int i=n-1;i>=0;i--){
+          vector<string> nex;
+          for(auto &x :mp[s[i]-'0']){
+              for(auto &i :cur){
+               nex.push_back(x + i);
+              }    
+          }
+          cur = nex;
+      }
+        
+     return cur;
     }
 };
