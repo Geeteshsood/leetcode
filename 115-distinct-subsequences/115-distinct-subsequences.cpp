c41eab23e7ec363fe8d9a319da2a97b8b9d3ce29
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-18 10:24:30 +0530

    Time: 82 ms (14.56%), Space: 18.5 MB (45.17%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    int lcs(int i,int j,string &s,string &t,vector<vector<int>> &dp){
+        
+        int m = s.length() , n = t.length();
+        
+        if(j == n){
+            return 1;
+        }
+        else if(i == m)return INT_MIN;
+        
+        if(dp[i][j] != -1)return dp[i][j];
+        
+        int val = 0;
+        
+        if(s[i] == t[j]){
+            int x = lcs(i+1,j+1,s,t,dp);
+            int y = lcs(i+1,j,s,t,dp);
+            
+            if(x != INT_MIN)val += x;
+            if(y != INT_MIN)val += y;
+        }
+        else{
+            int x = lcs(i+1,j,s,t,dp);
+            
+            if(x != INT_MIN)val += x;
+        }
+        
+      return dp[i][j] = val;  
+    }
+    
+    int numDistinct(string s, string t) {
+        
+       int m = s.length() , n = t.length();
+       vector<vector<int>> dp(m,vector<int>(n,-1));
+        
+       return lcs(0,0,s,t,dp);
+    }
+};
