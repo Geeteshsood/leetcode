7376f92eb8884f4960ba9d32e727aaa70c5dfa38
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-20 18:06:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (20.71%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    
-    bool find(int n){
-        
-        if(n == 1)return true;
-        
-        bool flag = false;
-        
-        if(n % 2 == 0)flag = flag || find(n / 2);
-        if(n % 3 == 0)flag = flag || find(n / 3);
-        if(n % 5 == 0)flag = flag || find(n / 5);
-        
-     return flag;
-    }
-    
     bool isUgly(int n) {
-      
-       if(n <= 0)return false;
-                   
-       return find(n);
+        
+        if(n <= 0)return false;
+        
+        while(n%2 == 0)n = n/2;
+        while(n%3 == 0)n = n/3;
+        while(n%5 == 0)n = n/5;       
+        
+     return n == 1;
     }
 };
