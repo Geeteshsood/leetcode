521810e3ddf029c2bdd26e4fccf96fe7b9f4538c
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-22 19:17:59 +0530

    Time: 432 ms (19.76%), Space: 21 MB (84.50%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
-    
-    unordered_map<int,unordered_map<int,int>> dp;
-    
-    bool find(unordered_set<int> &stones,int val,int jump,int &dest){
+    bool canCross(vector<int>& stones) {
         
-            // cout<<val<<" "<<jump<<endl;
-            if(val == dest)return true;
-            else if(val > dest)return false;
-
-           // cout<<val<<" "<<jump<<endl;
-            if(dp.count(val) && dp[val].count(jump))return dp[val][jump];
+        int n = stones.size();
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
         
-            bool flag = false;
-            val += jump;
+        if(stones[1]!=1)return false;
         
-            if(stones.count(val+1))flag = flag || find(stones,val+1,jump+1,dest);
-            if(stones.count(val))flag = flag || find(stones,val,jump,dest);    
-            if(jump-1>0 && stones.count(val-1))flag = flag || find(stones,val-1,jump-1,dest);    
+        if(n == 2)return true;
         
-            val-= jump;
+        dp[1][1] = true;
         
-     return dp[val][jump] = flag;
-    }
-    
-    bool canCross(vector<int>& stone) {
+        for(int i=2;i<n;i++){
+            
+            for(int j=1;j<i;j++){
+                
+                int k = stones[i]-stones[j];
+                
+                bool flag = false;
+                
+                if(k-1>=0 && k-1<n)flag = flag || dp[j][k-1];
+                if(k<n)flag = flag || dp[j][k];
+                if(k+1<n)flag = flag || dp[j][k+1];
+                
+                if(k<n)dp[i][k] = flag;
+                
+                if(i == n-1 && flag){
+                    return true;
+                }
+            }
+        }
         
-        int dest = stone.back();
-        unordered_set<int> stones(stone.begin(),stone.end());
-        
-        if(stone[1]!=1)return false;
-        
-        return find(stones,1,1,dest);
-        
+     return false;
     }
 };
