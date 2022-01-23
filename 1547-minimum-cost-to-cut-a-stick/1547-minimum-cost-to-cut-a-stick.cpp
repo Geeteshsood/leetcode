e1ad8496ca7848caf36220e2205301e7f0503b9a
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-23 19:14:37 +0530

    Time: 103 ms (34.03%), Space: 10.3 MB (31.51%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    
-    int find(int i,int j,int ls,int le,vector<int> &cuts,vector<vector<int>> &dp){
-        
-        if(i > j)return 0;
-        
-        if(dp[i][j] != -1)return dp[i][j];
-        
-        int ans = INT_MAX;
-        
-        for(int k=i;k<=j;k++){
-            
-            int x = find(i,k-1,ls,cuts[k],cuts,dp);
-            int y = find(k+1,j,cuts[k],le,cuts,dp);
-            
-            ans = min(ans,le-ls + x + y);
-        }
-        
-       return dp[i][j] = ans; 
-    }
-    
-    int minCost(int len, vector<int>& cuts) {
+    int minCost(int len, vector<int>& cuts){
         
         sort(cuts.begin(),cuts.end());
+        int n = cuts.size() , x = 0;
         
-        int n = cuts.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<vector<int>> dp(n,vector<int>(n));
         
-        return find(0,n-1,0,len,cuts,dp);
+        for(int g=0;g<n;g++){
+            for(int i=0,j=g;i<n && j<n;i++,j++){
+                
+                int val = 0 , ans = INT_MAX;
+                
+                for(int k=i;k<=j;k++){
+                    
+                    if(j<n-1 && i>0)x = cuts[j+1]-cuts[i-1];
+                    else if(i==0 && j==n-1)x = len;
+                    else if(i==0)x = cuts[j+1];
+                    else if(j==n-1)x = len-cuts[i-1];
+                    
+                    val = x;
+                    if(k-1>=0)val += dp[i][k-1];
+                    if(k+1<n)val += dp[k+1][j];
+                    
+                    ans = min(ans,val);
+                }
+                
+                dp[i][j] = ans;
+            }
+        }
+        
+    return dp[0][n-1];
     }
 };
