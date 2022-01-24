226634b8cabbe240b9570c3b1895a6f1d452b248
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-24 13:15:42 +0530

    Time: 15 ms (9.23%), Space: 10 MB (18.68%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
+    
+     unordered_set<int> s;
+    
+    int find(int i,vector<int> &costs,vector<int> &dp){
+      
+        if(i > 365)return 0;
+        
+        if(dp[i] != -1)return dp[i];
+        
+        int val = 0;
+        
+        if(s.count(i)){
+    val = min({costs[0] + find(i+1,costs,dp) , costs[1] + find(i+7,costs,dp) , costs[2] + find(i+30,costs,dp)});
+        }
+        else val = find(i+1,costs,dp);
+         
+      return dp[i] = val;
+    }
+    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         
-        int n = days.size() , idx = 0;
+        vector<int> dp(366,-1);
         
-        vector<int> dp(366);
-        
-        for(int i=1;i<=365;i++){
-            
-            if(idx<n && i == days[idx]){
-                
-                int val1 = 0 , val2 = 0;
-                
-                if(i>=30)val1 = dp[i-7] , val2 = dp[i-30];
-                else if(i>=7)val1 = dp[i-7];
-                
-                dp[i] = min({costs[0] + dp[i-1] , costs[1] + val1 , costs[2] + val2});
-                
-                idx++;
-            }
-            else dp[i] = dp[i-1];
+        for(auto &i:days){
+            s.insert(i);
         }
         
-       return dp[365];
+        return find(0,costs,dp);
     }
 };
