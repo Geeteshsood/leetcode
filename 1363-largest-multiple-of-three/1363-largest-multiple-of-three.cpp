6dfeda7a62d343fd2e1ab94c158b4db9755759ed
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-25 13:44:20 +0530

    Time: 104 ms (9.19%), Space: 20.1 MB (33.51%) - LeetHub

--- 1363-largest-multiple-of-three/1363-largest-multiple-of-three.cpp
+++ 1363-largest-multiple-of-three/1363-largest-multiple-of-three.cpp
@@ -5,52 +5,27 @@ public:
         string zero,one,two;
         sort(digits.rbegin(),digits.rend());
         
+        int sum = 0;
+        
         for(auto &i : digits){
+           sum += i;
            if(i%3 == 0)zero += i + '0';
            else if(i%3 == 1)one += i + '0'; 
            else if(i%3 == 2)two += i + '0'; 
         }
         
-        int x = one.size() , y = two.size();
-        int z = min(x,y);
-        int a = 3*((x-z)/3) , b = 3*((y-z)/3);
-        
-        if(z == 0){
-            zero += one.substr(0,a) + two.substr(0,b);
+        if(sum % 3 == 1){
+            if(one.size())one = one.substr(0,one.size()-1);
+            else two = two.substr(0,two.size()-2);
         }
-        else if(x == y){
-            zero += one + two;
-        }
-        else if(x > y){
-            if((x-z)%3 == 0){
-                zero += one + two;
-            }
-            else if((x-z)%3 == 1){
-                zero+=two + one.substr(0,z);
-                zero += one.substr(z,a);
-            }
-            else if((x-z)%3 == 2){
-                zero += two.substr(0,z-1) + one.substr(0,z-1);
-                int a = 3*((x-z+1)/3);
-                zero += one.substr(z-1,a);
-            }    
-        }
-        else{
-            if((y-z)%3 == 0){
-                zero += one + two;
-            }
-            else if((y-z)%3 == 1){
-                zero+=one+two.substr(0,z);
-                zero += two.substr(z,b);
-            }
-            else if((y-z)%3 == 2){
-                zero += one.substr(0,z-1) + two.substr(0,z-1);
-                int a = 3*((y-z+1)/3);
-                zero += two.substr(z-1,a);
-            }  
+        else if(sum % 3 == 2){
+            if(two.size())two = two.substr(0,two.size()-1);
+            else one = one.substr(0,one.size()-2); 
         }
         
-         int n = zero.size();
+        zero+=one + two;
+        
+        int n = zero.size();
         if(n > 1 && zero[0] == zero[n-1] && zero[0] == '0')return "0";
         
         sort(zero.rbegin(),zero.rend());
