13adf003f36ecd977a74743c8d147c47cbcdd457
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-26 10:00:33 +0530

    Time: 194 ms (40.80%), Space: 84.4 MB (79.25%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,59 +1,26 @@
 class Solution {
 public:
     
-    vector<int> res1,res2;
-    
-    void find1(TreeNode* root1){
+    void find(TreeNode* root,vector<int> &res){
         
-        if(root1 == NULL){
+        if(root == NULL){
             return ;
         }
         
-        find1(root1->left);
-        res1.push_back(root1->val);
-        find1(root1->right);
-    }
-    
-     void find2(TreeNode* root2){
-        
-        if(root2 == NULL){
-            return ;
-        }
-        
-        find2(root2->left);
-        res2.push_back(root2->val);
-        find2(root2->right);
+        find(root->left,res);
+        res.push_back(root->val);
+        find(root->right,res);
     }
     
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         
-        find1(root1);
-        find2(root2);
+        vector<int> res;
         
-        int m = res1.size() , n = res2.size();
-        vector<int> ans;
+        find(root1,res);
+        find(root2,res);
+
+        sort(res.begin(),res.end());
         
-        int i = 0 , j = 0;
-        
-        while(i < m && j < n){
-            
-            ans.push_back(min(res1[i],res2[j]));
-            
-            if(res1[i] < res2[j])i++;
-            else j++;
-            
-        }
-        
-        while(i<m){
-            ans.push_back(res1[i]);
-            i++;
-        }
-        
-        while(j<n){
-            ans.push_back(res2[j]);
-            j++;
-        }
-        
-        return ans;
+        return res;
     }
 };
