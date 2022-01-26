034460e5cc4f3323d011b399b8cab35e20bcdaee
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-26 11:48:29 +0530

    Time: 287 ms (12.25%), Space: 84.6 MB (67.43%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,26 +1,57 @@
 class Solution {
 public:
-    
-    void find(TreeNode* root,vector<int> &res){
-        
-        if(root == NULL){
-            return ;
-        }
-        
-        find(root->left,res);
-        res.push_back(root->val);
-        find(root->right,res);
-    }
-    
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         
-        vector<int> res;
+        vector<int> ans;
         
-        find(root1,res);
-        find(root2,res);
-
-        sort(res.begin(),res.end());
+        TreeNode* root = NULL;
         
-        return res;
+        root = root1;
+        
+        stack<TreeNode*> st1,st2;
+        
+        while(root){
+            st1.push(root);
+            root = root->left;
+        }
+        
+        root = root2;
+        
+        while(root){
+            st2.push(root);
+            root = root->left;
+        }
+        
+        TreeNode *node = NULL;
+        
+        while(true){
+           if(st1.empty() && st2.empty())break;
+          
+            
+           if( !st1.empty() && (st2.empty() || st1.top()->val < st2.top()->val)){
+               node = st1.top();
+               st1.pop();
+               ans.push_back(node->val);
+               node = node->right;
+               
+               while(node){
+                   st1.push(node);
+                   node = node->left;
+               }
+           }
+           else if(!st2.empty() && (st1.empty() || st2.top()->val <= st1.top()->val)){
+               node = st2.top();
+               st2.pop();
+               ans.push_back(node->val);
+               node = node->right;
+               
+               while(node){
+                   st2.push(node);
+                   node = node->left;
+               }
+           }
+        }
+      
+        return ans;
     }
 };
