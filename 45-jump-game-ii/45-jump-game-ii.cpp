83e9371798abbbcc4635a0fe35963a6510eacb90
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-26 14:49:21 +0530

    Time: 25 ms (56.18%), Space: 16.5 MB (42.93%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -3,28 +3,21 @@ public:
     int jump(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n,INT_MAX);
         
-        dp[0] = nums[0];
+        if(n == 1)return 0;
         
-        for(int i=1;i<n;i++){
+        int jump = 1, maxReach = nums[0] , curReach = nums[0];
+        
+        for(int i=1;i<n-1;i++){
             
-            int val = i + nums[i];
+            maxReach = max(maxReach , i + nums[i]);
             
-            auto jmp = lower_bound(dp.begin(),dp.end(),i)-dp.begin();// tell us we can reach that point
-                                                                     // with min jumps
-            if(i == n-1)return jmp + 1;
-            
-            if(dp[jmp + 1] == INT_MAX && val > dp[jmp]){
-                dp[jmp + 1] = val;
-            }
-            else if(dp[jmp + 1] != INT_MAX && val > dp[jmp+1]){
-                dp[jmp + 1] = val;
+            if(i == curReach){
+                jump++;
+                curReach = maxReach;
             }
         }
-        // for(int i=0;i<n;i++){
-        //     cout<<dp[i]<<" ";
-        // }
-        return 0;
+        
+        return jump;
     }
 };
