6bc2348a45ee342343e0f857707be6b01a0e0eae
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-26 15:14:53 +0530

    Time: 68 ms (21.00%), Space: 36.2 MB (21.47%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     
-    bool find(int i,vector<int> &arr,vector<bool> &vis){
+    bool find(int i,vector<int> &arr,vector<bool> &vis,vector<int> &dp){
         
         if(i < 0 || i >= arr.size() || vis[i])return false;
         
         vis[i] = true;
         
+        if(dp[i] != -1)return dp[i];
+        
         if(arr[i] == 0)return true;
         
-        return find(i + arr[i],arr,vis) || find(i - arr[i],arr,vis);
+        return dp[i] = find(i + arr[i],arr,vis,dp) || find(i - arr[i],arr,vis,dp);
     }
     
     bool canReach(vector<int>& arr, int start) {
         
         int n = arr.size();
         vector<bool> vis(n);
+        vector<int> dp(n,-1);
         
-        return find(start,arr,vis);
+        return find(start,arr,vis,dp);
     }
 };
