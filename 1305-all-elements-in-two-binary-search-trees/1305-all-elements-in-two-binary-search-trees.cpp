a6c358d6d0074f22601d09d67d28ebc49655ced4
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-26 12:11:19 +0530

    Time: 332 ms (5.01%), Space: 84.3 MB (97.28%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -1,54 +1,44 @@
 class Solution {
 public:
+    
+    void push_left(TreeNode* root,stack<TreeNode*> &st){
+        
+        if(root == NULL)return ;
+        
+        st.push(root);
+        push_left(root->left,st);
+    }
+    
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         
         vector<int> ans;
         
-        TreeNode* root = NULL;
-        
-        root = root1;
-        
         stack<TreeNode*> st1,st2;
         
-        while(root){
-            st1.push(root);
-            root = root->left;
-        }
-        
-        root = root2;
-        
-        while(root){
-            st2.push(root);
-            root = root->left;
-        }
+        push_left(root1,st1);
+        push_left(root2,st2);
         
         TreeNode *node = NULL;
         
-        while(true){
-           if(st1.empty() && st2.empty())break;
-          
+        while(st1.size() || st2.size()){
             
            if( !st1.empty() && (st2.empty() || st1.top()->val < st2.top()->val)){
                node = st1.top();
                st1.pop();
+               
                ans.push_back(node->val);
                node = node->right;
                
-               while(node){
-                   st1.push(node);
-                   node = node->left;
-               }
+               push_left(node,st1);
            }
            else if(!st2.empty() && (st1.empty() || st2.top()->val <= st1.top()->val)){
                node = st2.top();
                st2.pop();
+               
                ans.push_back(node->val);
                node = node->right;
                
-               while(node){
-                   st2.push(node);
-                   node = node->left;
-               }
+               push_left(node,st2);
            }
         }
       
