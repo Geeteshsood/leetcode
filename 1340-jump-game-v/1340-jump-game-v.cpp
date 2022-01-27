a60bfb80bd77b51b8f964875e4a92ccb78f3d0f3
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-27 11:38:43 +0530

    Time: 66 ms (37.47%), Space: 14.9 MB (78.39%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     
-    int find(int i,int d,vector<int> &arr,vector<bool> &vis,vector<int> &dp){
+    int find(int i,int d,vector<int> &arr,vector<int> &dp){
         
         int n = arr.size();
         
-        if(i < 0 || i >= n|| vis[i])return INT_MIN;
+        if(i < 0 || i >= n)return INT_MIN;
         
         if(dp[i] != -1){
             return dp[i];
         }
-        
-        vis[i] = true;
         
         int x = INT_MIN , y = INT_MIN;
         
@@ -19,7 +17,7 @@ public:
         
         for(int j=i+1;j<=lim;j++){
               if(arr[i] > arr[j]){
-                 x = max(x , find(j,d,arr,vis,dp));
+                 x = max(x , find(j,d,arr,dp));
               }
               else break;
         }
@@ -28,12 +26,10 @@ public:
         
         for(int j=i-1;j>=lim;j--){
               if(arr[i] > arr[j]){
-                  y = max(y , find(j,d,arr,vis,dp));
+                  y = max(y , find(j,d,arr,dp));
               }
               else break;
         }
-        
-        vis[i] = false;
         
         x = max(x,0),y = max(y,0);
 
@@ -44,14 +40,11 @@ public:
         
         int n = arr.size() , ans = INT_MIN;
         
-        vector<bool> vis(n,false);
         vector<int> dp(n,-1);
         
         for(int i=0;i<n;i++){
             
-            if(vis[i])continue;
-            
-            int x = find(i,d,arr,vis,dp);
+            int x = find(i,d,arr,dp);
             ans = max(ans,x);         
         }
         
