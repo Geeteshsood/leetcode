5ed7f4c49432cada5b07f2b8fc5c0cad2540d179
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-29 20:28:36 +0530

    Time: 216 ms (65.89%), Space: 85.2 MB (23.84%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -12,32 +12,23 @@ public:
             dp[i] = total;
         }
 
-        int cnt = 0 ,left = 0  , sum = 0;
+        int cnt = 0 , left = 0 , sum = 0 , val = 0;
         
         for(int i=0;i<n-2;i++){
             
-            left += nums[i];
-            int val = 2*left;
+            left = dp[i];
+            val = 2*left;
             
-            int j = lower_bound(dp.begin() + i + 1,dp.end(),val) - dp.begin();
+            int j = lower_bound(dp.begin()+i+1,dp.end(),val) - dp.begin();
             
             if(j > n-2)break;
             
-            sum = total - left;
-            sum = sum/2;
+            val = left + (total - left)/2;
             
-            val = left + sum;
+            int k = upper_bound(dp.begin()+i+1,dp.begin() + n-1,val) - dp.begin();
             
-            int k = upper_bound(dp.begin() + i + 1,dp.begin() + n-2,val) - dp.begin();
+            k--;
             
-            while(k > i && true){
-               int mid = dp[k]-dp[i];
-               int right = total - left - mid;
-               if(mid <= right)break;
-               k--;
-            }
-            
-            // cout<<i<<" "<<j<<" "<<k<<endl;
             if(k>=j)cnt = (cnt + k-j + 1)%mod;
         }
         
