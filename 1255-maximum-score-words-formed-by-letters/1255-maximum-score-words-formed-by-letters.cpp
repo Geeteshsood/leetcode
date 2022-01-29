f6f520c7dc2b64511147d2d219723e60f162fba4
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-29 11:07:25 +0530

    Time: 7 ms (75.88%), Space: 16 MB (57.14%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,68 +1,45 @@
 class Solution {
 public:
-    
-    unordered_map<int,int> dp[15];
-    
-    bool isValid(int i,unordered_map<int,int> &mp){
+
+    int find(int i,vector<int> &score,vector<int> &cnt,vector<string> &words){
         
-          for(auto [x,y] : dp[i]){
-              if(mp[x] < y){
-                  return false;
-              }
-          }
-        
-        return true;
-    }
-    
-    int find(int i,int n,vector<int> &score,unordered_map<int,int> &mp){
-        
-        if(i == n)return 0;
+        if(i == words.size())return 0;
         
          int x  = 0 , y = 0;
         
-         if(isValid(i,mp)){
-             for(auto [x,y] : dp[i]){
-               if(mp[x] == y)mp.erase(x);
-               else mp[x]-= y;
-             }
-             
-             x = score[i] + find(i+1,n,score,mp);
-             
-             for(auto [x,y] : dp[i]){
-                  mp[x]+=y;
+         y = find(i+1,score,cnt,words);
+        
+         vector<int> count(cnt.begin(),cnt.end());
+        
+         bool flag = true;
+         int sum = 0;
+        
+         for(auto &x : words[i]){
+             count[x-'a']--;
+             sum += score[x-'a'];
+             if(count[x-'a'] < 0){
+                 flag = false;
+                 break;
              }
          }
-         y = find(i+1,n,score,mp);
         
+         if(flag){
+             x = sum + find(i+1,score,count,words);
+         }
         
      return max(x,y); 
     }
     
-    
-    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& v) {
+    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
         
         int n = words.size();
-        
-        vector<int> score(n);
-        
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(auto &x : words[i]){
-                sum += v[x-'a'];
-                dp[i][x]++;
-            }
-            score[i] = sum;
-        }
-        // for(int i=0;i<n;i++){
-        //     cout<<score[i]<<" ";
-        // }
-        unordered_map<int,int> mp;
-        
+    
+        vector<int> cnt(26);
         for(auto &x : letters){
-            mp[x]++;
+            cnt[x-'a']++;
         }
         
-        return find(0,n,score,mp);
+        return find(0,score,cnt,words);
         
     }
 };
