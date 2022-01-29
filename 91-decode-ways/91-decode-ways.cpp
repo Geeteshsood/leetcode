5f3539403075876010cf000ce36c363e7d832842
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-29 13:05:48 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (69.85%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    
-	 int find(int i,string &s,vector<int> &dp){
-        
-        if(i == s.size())return 1;
-        
-        int sum = 0 , val = 0;
-        
-        if(dp[i] != -1)return dp[i];
-        
-        for(int j=i;j<s.size();j++){
-            
-            val = val*10 + s[j]-'0';
-            
-            if(val < 1 || val > 26)break;
-                
-            sum += find(j + 1,s,dp);
-        }
-        
-      return dp[i] = sum;  
-    }
-    
     int numDecodings(string s) {
         
-         vector<int> dp(s.size(),-1);
-		   
-		 return find(0,s,dp);
+        int n = s.size() , val = 0;
+        vector<int> dp(n + 1);
+        
+        if(s[0] != '0')dp[0] = 1;
+        
+        for(int i=1;i<=n;i++){
+            
+             if(s[i-1] != '0')dp[i] = dp[i-1];
+            
+            if(i>=2 && s[i-2] != '0'){ 
+                val = (s[i-1]-'0') + (s[i-2]-'0')*10;
+            
+                if(val >=1 && val <= 26){
+                    dp[i] += dp[i-2];
+                }
+            }    
+        }
+
+        return dp[n];
     }
+    
 };
