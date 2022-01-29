fefea32a743b428d94dbc7c707e3e8095126f0e9
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-29 20:13:23 +0530

    Time: 272 ms (50.77%), Space: 85.1 MB (41.67%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     
-    int find(int i,int j,int sum,vector<int> &nums,vector<int> &dp){
-        
-        int n = nums.size(),ans = -1 ,mid = 0;
-        int start = j, end = n-2;
-        
-        while(start<=end){
-            
-            mid = start + (end-start)/2;
-            
-            int left = dp[mid] - dp[i];
-            int right = sum - left;
-            
-            if(left<=right){
-                ans = mid;
-                start = mid + 1;
-            }
-            else end = mid-1;
-        }
-        
-        return ans;
-    }
-    
     int waysToSplit(vector<int>& nums) {
         
         int mod = 1e9 + 7;
@@ -33,18 +11,34 @@ public:
             total += nums[i];
             dp[i] = total;
         }
-        
-        int cnt = 0 , j = 0 ,left = 0;
+
+        int cnt = 0 ,left = 0  , sum = 0;
         
         for(int i=0;i<n-2;i++){
             
             left += nums[i];
             int val = 2*left;
             
-            j = lower_bound(dp.begin() + i + 1,dp.end(),val) - dp.begin();
-
-            int k = find(i,j,total - dp[i],nums,dp);
-            if(k != -1)cnt = (cnt + k - j + 1)%mod;
+            int j = lower_bound(dp.begin() + i + 1,dp.end(),val) - dp.begin();
+            
+            if(j > n-2)break;
+            
+            sum = total - left;
+            sum = sum/2;
+            
+            val = left + sum;
+            
+            int k = upper_bound(dp.begin() + i + 1,dp.begin() + n-2,val) - dp.begin();
+            
+            while(k > i && true){
+               int mid = dp[k]-dp[i];
+               int right = total - left - mid;
+               if(mid <= right)break;
+               k--;
+            }
+            
+            // cout<<i<<" "<<j<<" "<<k<<endl;
+            if(k>=j)cnt = (cnt + k-j + 1)%mod;
         }
         
        return cnt; 
