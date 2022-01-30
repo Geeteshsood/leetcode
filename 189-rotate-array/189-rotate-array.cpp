787e6cec409bf2012e65560707f5ce5d0dfe54d9
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-01-30 16:24:11 +0530

    Time: 38 ms (46.57%), Space: 25 MB (52.93%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -7,13 +7,7 @@ public:
         
         k = k%n;
         
-        for(int i=n-k;i<n;i++){
-            v.push_back(nums[i]);
-        }
-        for(int i=0;i<n-k;i++){
-            v.push_back(nums[i]);
-        }
+        ::rotate(nums.begin(),nums.begin() + n-k , nums.end());
         
-        nums = v;
     }
 };
