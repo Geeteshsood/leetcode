0dac89e010b173d6e64e02e9d528dc58b47d7bed
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-01 18:36:44 +0530

    Time: 20 ms (12.90%), Space: 9.6 MB (57.13%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-           int n = nums.size();
-           vector<int> v(32);
+        int n = nums.size();
+      
+        int val = 0;
         
+        for(int j=0;j<32;j++){
+           
+           int cnt = 0;
+            
            for(int i=0;i<n;i++){
-                int num = nums[i];
                
-                for(int j=0;j<32;j++){
-                    
-                    if(num & (1<<j)){
-                        v[j]++;
-                    }
-                    
+                if(nums[i] & (1<<j)){
+                       cnt++;
                 }
            }
-        
-           int val = 0;
-        
-           for(int j=0;j<32;j++){
-                 if(v[j]%3!=0){
-                     val += (1<<j);
-                 }
-           }
+            
+            if(cnt%3){
+               val += (1<<j);
+            } 
+        }
         
         return val; 
     }
