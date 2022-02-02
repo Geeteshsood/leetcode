388718c4515df057b0464bf4dffc82cb6812a296
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-02 10:27:53 +0530

    Time: 26 ms (44.23%), Space: 8.6 MB (90.96%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -5,31 +5,27 @@ public:
         int m = s.size() , n = p.size();
         vector<int> ans;
         
-        map<char,int> mp1,mp2;
+        vector<int> mp1(26,0);
+        vector<int> mp2(26,0);
         
         if(n > m)return {};
         
         for(int i=0;i<n;i++){
-            mp2[p[i]]++;
+            mp2[p[i]-'a']++;
         }
         
          for(int i=0;i<n;i++){
-            mp1[s[i]]++;
+            mp1[s[i]-'a']++;
         }
         
         if(mp1 == mp2)ans.push_back(0);
         
         for(int i=n;i<m;i++){
             
-            if(mp1[s[i-n]] == 1){
-                mp1.erase(s[i-n]);
-            }
-            else mp1[s[i-n]]--;
-            
-            mp1[s[i]]++;
+            mp1[s[i-n]-'a']--;
+            mp1[s[i]-'a']++;
             
             if(mp1 == mp2)ans.push_back(i-n+1);
-            
         }
       
         return ans;
