9579702a469615834c4d120ba7ab306d938ed927
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-02 18:12:50 +0530

    Time: 4 ms (49.47%), Space: 10.2 MB (9.51%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.cpp
+++ 397-integer-replacement/397-integer-replacement.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     
+    unordered_map<int,int> mp;
+    
     int integerReplacement(int n) {
         
         if(n == INT_MAX)return 32;
         if(n == 1)return 0;
         
-        
-        if(n%2 == 0){
-            return 1 + integerReplacement(n/2);
+        if(mp.count(n)){
+            return mp[n];
         }
         
-        return 1 + min(integerReplacement(n+1) , integerReplacement(n-1));
+        if(n%2 == 0){
+            return mp[n] = 1 + integerReplacement(n/2);
+        }
+        
+        return mp[n] = 1 + min(integerReplacement(n+1) , integerReplacement(n-1));
     }
 };
