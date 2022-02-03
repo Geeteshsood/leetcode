c16385c67d18c16e96c387221ca4db9ae5045cff
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-03 19:35:17 +0530

    Time: 6 ms (94.59%), Space: 6.5 MB (24.15%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     
-    vector<int> row,col,box;
+    int row[10],col[10],box[10];
     
-    void find(vector<vector<char>>& board){
+    void generate(vector<vector<char>>& board){
          
         for(int i=0;i<9;i++){
             int rmask = 0 , cmask = 0;
@@ -35,47 +35,37 @@ public:
         if(c == 9){
             if(r == 8)return true;
             
-            if(solve(r+1,0,board))return true;
-            else return false;
+            return solve(r+1,0,board);
         }
         
         if(board[r][c] != '.'){
-            if(solve(r,c+1,board))return true;
+            return solve(r,c+1,board);
         }
-        else{
             
-          int k = 3*(r/3) + (c/3);
+        int k = 3*(r/3) + (c/3);
+          
+        for(int n=1;n<10;n++){
             
-          for(int n=1;n<10;n++){
-              
-              bool flag = false;
-              
-              flag = flag || (row[r] & (1<<n));
-              flag = flag || (col[c] & (1<<n));
-              flag = flag || (box[k] & (1<<n));
-              
-              
-              if(!flag){
-                  
-                  row[r] = row[r] | (1<<n);
-                  col[c] = col[c] | (1<<n);
-                  box[k] = box[k] | (1<<n);
-                  
-                  board[r][c] = n + '0';
-                  
-                  // cout<<r<<" "<<c<<" "<<n<<endl;
-                  if(solve(r,c+1,board)){
-                      return true;
-                  }
-                  
-                  board[r][c] = '.';
-                  
-                  row[r] = row[r] & (~(1<<n));
-                  col[c] = col[c] & (~(1<<n));
-                  box[k] = box[k] & (~(1<<n));
-                  
-              }
-          }
+            bool flag = (row[r] & (1<<n)) || (col[c] & (1<<n)) || (box[k] & (1<<n)); // checking if bit is found
+            
+            if(!flag){
+                
+                row[r] ^= (1<<n);        // setting bit
+                col[c] ^= (1<<n);
+                box[k] ^= (1<<n);
+                
+                board[r][c] = n + '0';
+                
+                if(solve(r,c+1,board)){
+                    return true;
+                }
+                
+                board[r][c] = '.';
+                
+                row[r] ^= (1<<n);
+                col[c] ^= (1<<n);          // unsetting bit.
+                box[k] ^= (1<<n);
+            }
         }
         
         return false;
@@ -83,17 +73,11 @@ public:
     
     void solveSudoku(vector<vector<char>>& board) {
         
-        row = vector<int> (10);
-        col = vector<int> (10);
-        box = vector<int> (10);
+        memset(row,0,sizeof(row));
+        memset(col,0,sizeof(col));
+        memset(box,0,sizeof(box));
         
-        find(board);
-        
-        // for(int i=0;i<9;i++){
-        //     cout<<row[i]<<" "<<col[i]<<" "<<box[i]<<endl;
-        // }
-        // cout<<endl;
-        
+        generate(board);
         solve(0,0,board);
         
     }
