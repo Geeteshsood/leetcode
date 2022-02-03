7de8bad5621c0f7e6657b84a33cb290fd1808280
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-03 19:25:07 +0530

    Time: 10 ms (90.77%), Space: 6.5 MB (55.45%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,99 +1,100 @@
-// 5 3 4 6 7 8 1 9 2 
-// 6 7 2 1 9 5 3 4 8 
-// 1 9 8 2 3 4 5 6 7 
-// 8 5 9 7 6 1 4 2 3 
-// 4 . . 8 . 3 . . 1 
-// 7 . . . 2 . . . 6 
-// . 6 . . . . 2 8 . 
-// . . . 4 1 9 . . 5 
-// . . . . 8 . . 7 9 
 class Solution {
 public:
     
-    bool isValid(int dig,int r,int c,vector<vector<char>> &board){
-         // bool flag = false;
-         // if(r >=3 && board[0][2] == '4' && board[1][1] == '7' && board[3][1] == '5' && board[3][3] == '7' && board[3][2] == '9' && board[3][7] == '2'){
-         //     flag = true;
-         // 
-        
-        // if(flag)cout<<r<<" , "<<c<<endl;
-        
-        char val = dig + '0';
+    vector<int> row,col,box;
     
+    void find(vector<vector<char>>& board){
+         
         for(int i=0;i<9;i++){
-             
-                 // if(flag){
-                 //     cout<<board[r][i]<<" "<<board[i][c]<<" "<<val<<endl;
-                 // }
-             
-            if(board[r][i] == val || board[i][c] == val)return false;
-        }
-        
-        int x = 3*(r/3) , y = 3*(c/3);
-        
-        for(int i=x;i<x+3;i++){
-            for(int j=y;j<y+3;j++){
-                if(board[i][j] == val){
-                    return false;
-                }
+            int rmask = 0 , cmask = 0;
+            for(int j=0;j<9;j++){
+                if(board[i][j] != '.')rmask = rmask | (1<<(board[i][j] - '0'));
+                if(board[j][i] != '.')cmask = cmask | (1<<(board[j][i] - '0'));
             }
+            row[i] = rmask;
+            col[i] = cmask;
         }
         
-        return true;
+        int cnt = 0;
+        for(int r=0;r<9;r+=3){
+          for(int c=0;c<9;c+=3){ 
+             int bmask = 0;
+             for(int i=r;i<r+3;i++){
+                 for(int j=c;j<c+3;j++){
+                     if(board[i][j] != '.')bmask = bmask | (1<<(board[i][j] - '0'));
+                 }
+             }
+             box[cnt] = bmask;
+             cnt++;
+          }
+        }        
     }
     
-    bool find(int r,int c,vector<vector<char>>& board){
+    bool solve(int r,int c,vector<vector<char>> &board){
         
-           if(r==8 && c==8)return true;
-//         if(r >=3 && board[0][2] == '4' && board[1][1] == '7' && board[3][1] == '5' && board[3][3] == '7' && board[3][2] == '9' && board[3][7] == '2'){
+        if(c == 9){
+            if(r == 8)return true;
             
-//            for(int i=0;i<9;i++){
-//                for(int j=0;j<9;j++){
-//                    cout<<board[i][j]<<" ";
-//                }
-//                cout<<endl;
-//            }
+            if(solve(r+1,0,board))return true;
+            else return false;
+        }
+        
+        if(board[r][c] != '.'){
+            if(solve(r,c+1,board))return true;
+        }
+        else{
             
+          int k = 3*(r/3) + (c/3);
+            
+          for(int n=1;n<10;n++){
+              
+              bool flag = false;
+              
+              flag = flag || (row[r] & (1<<n));
+              flag = flag || (col[c] & (1<<n));
+              flag = flag || (box[k] & (1<<n));
+              
+              
+              if(!flag){
+                  
+                  row[r] = row[r] | (1<<n);
+                  col[c] = col[c] | (1<<n);
+                  box[k] = box[k] | (1<<n);
+                  
+                  board[r][c] = n + '0';
+                  
+                  // cout<<r<<" "<<c<<" "<<n<<endl;
+                  if(solve(r,c+1,board)){
+                      return true;
+                  }
+                  
+                  board[r][c] = '.';
+                  
+                  row[r] = row[r] & (~(1<<n));
+                  col[c] = col[c] & (~(1<<n));
+                  box[k] = box[k] & (~(1<<n));
+                  
+              }
+          }
+        }
         
-//            cout<<r<<" "<<c<<" *** "<<endl;
-//         }
-        
-           if(c == 8)c=0,r++;
-        
-           // if(r >=3 && board[0][2] == '4' && board[1][1] == '7' && board[3][1] == '5' && board[3][3] == '7' && board[3][2] == '9' && board[3][7] == '2'){cout<<r<<" , "<<c<<" *** "<<endl;}
-               // bool flag = true;
-        
-           for(int i=r;i<9;i++){
-               for(int j=c;j<9;j++){
-                   
-                   if(board[i][j] != '.'){
-                       if(j == 8)j=-1,i++;
-                       if(i == 9)return true;
-                       continue;
-                   }
-                   
-                   // flag = false;
-                   for(int dig=1;dig<10;dig++){
-          
-                       if(isValid(dig,i,j,board)){
-                           
-                           board[i][j] = dig + '0';
-                           
-                           if(find(i,j,board))return true;
-                           
-                           board[i][j] = '.';
-                       }
-                   }
-                   return false;
-               }
-           }
-        
-      return true;  
+        return false;
     }
     
     void solveSudoku(vector<vector<char>>& board) {
         
-          find(0,0,board);
-         
+        row = vector<int> (10);
+        col = vector<int> (10);
+        box = vector<int> (10);
+        
+        find(board);
+        
+        // for(int i=0;i<9;i++){
+        //     cout<<row[i]<<" "<<col[i]<<" "<<box[i]<<endl;
+        // }
+        // cout<<endl;
+        
+        solve(0,0,board);
+        
     }
 };
