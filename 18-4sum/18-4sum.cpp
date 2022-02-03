d3a75a3ff4e674c980c0d4402097f28d24992a40
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-03 09:46:47 +0530

    Time: 140 ms (35.29%), Space: 13 MB (91.17%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>> pi;
     
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
              sort(nums.begin(),nums.end());
 
-             set<pi> s;
-        
              vector<vector<int>> ans;
         
-             int n = nums.size();
+             int n = nums.size() , i = 0;
         
-            for(int i=0;i<n;i++){
-                for(int j=i+1;j<n;j++){
+            while(i<n){
+                
+                int j = i+1;
+                while(j<n){
                     
                     int x = j+1 , y = n-1;
                     
                     while(x < y){
                         
-                       int left = nums[i] + nums[j];
+                       int  left = nums[i] + nums[j];
                        int right = nums[x] + nums[y];
                        
                        if(left == target - right){
-                           if(!s.count({nums[i],{nums[j],nums[x]}})){
-                               ans.push_back({nums[i],nums[j],nums[x],nums[y]});
-                               s.insert({nums[i],{nums[j],nums[x]}});
-                           }
-                           x++;
+                           ans.push_back({nums[i],nums[j],nums[x],nums[y]});
                        }
-                       else if(left < target - right)x++;
-                       else y--;
-                    
+                        
+                       if(left <= target - right){
+                           x++;
+                           while( x<y && nums[x] == nums[x-1])x++;
+                       }
+                       else{
+                           y--;
+                           while( x<y && nums[y] == nums[y+1])y--;;
+                       }
                     } 
+                    j++;
+                    while( j<n && nums[j] == nums[j-1]){
+                        j++;
+                    }
                 }
+                i++;
+              while(i<n && nums[i] == nums[i-1])i++;  
             }
         
         return ans;
