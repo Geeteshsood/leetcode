4b5c58f24310d8474624eeb7ef4339d6e6b27552
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-07 19:17:12 +0530

    Time: 4 ms (47.41%), Space: 5.9 MB (81.41%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,22 +2,11 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         
-        
-        for(int i=0,j=31;i<16;i++,j--){
-            
-            bool seti = n & (1 << i);
-            bool setj = n & (1 << j);
-            
-            if(seti){
-                n = n | (1<<j);
-            }
-            else n = n & ~(1<<j);
-            
-            if(setj){
-                n = n | (1<<i);
-            }
-            else n = n & ~(1<<i);
-        }
+         n = ((n & 0x0000ffff)<<16) | ((n & 0xffff0000)>>16);
+          n = ((n & 0x00ff00ff)<<8) | ((n & 0xff00ff00)>>8);
+          n = ((n & 0x0f0f0f0f)<<4) | ((n & 0xf0f0f0f0)>>4);
+          n = ((n & 0x33333333)<<2) | ((n & 0xcccccccc)>>2);
+          n = ((n & 0x55555555)<<1) | ((n & 0xaaaaaaaa)>>1);
         
         return n;
     }
