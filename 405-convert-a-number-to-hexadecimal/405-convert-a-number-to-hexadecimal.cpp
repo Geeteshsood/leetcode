c65838b7e725b8caf614d9178f2cb907837298db
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-09 14:42:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (60.70%) - LeetHub

--- 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.cpp
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-    string toHex(int num){
+    string toHex(int num) {
         
-        string str;
-        
-        string res = "0123456789abcdef";
-         
-        for(int bit=4;bit<32;bit+=4){
-            
-             int x = 1<<(bit);
-             x = x - 1;
-             x = num & x;
-             x = x>>(bit-4);
-            
-             if(x<0)x = x^(0xfffffff0);
-            
-             str += res[x];
-        }  
-        
-        int x = ~0;
-        x = x & num;
-        
-        x = x>>28;
-        if(x<0)x = x^(0xfffffff0);
-        
-        str += res[x];
-        
-        reverse(str.begin(),str.end());
+        string ans,str = "0123456789abcdef";
         
         for(int i=0;i<8;i++){
-            if(str[i] != '0'){
-               return str.substr(i);
+           
+           ans = str[num & (0xf)] + ans;
+           num = num>>4; 
+        }
+        
+        for(int i=0;i<8;i++){
+            if(ans[i] != '0'){
+                return ans.substr(i);
             }
         }
         
