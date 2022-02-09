b2dc979a839aba50761d718da54484e9c45af7fe
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-09 11:26:57 +0530

    Time: 3 ms (73.41%), Space: 6.3 MB (57.03%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -4,27 +4,29 @@ public:
     string addBinary(string a, string b) {
         
         string str;
-        int m = a.size() , n  = b.size() , carry = 0;
-        int x = 0 , y = 0,i = m-1 , j = n-1;
+        int carry = 0 , sum = 0;
+        int i = a.size()-1 , j = b.size()-1;
         
-        while(i >=0 || j >=0){
+        while(i >=0 || j >=0 || carry){
             
-             // cout<<i<<" "<<j<<" "<<carry<<endl;
+             int x = 0 , y = 0;
             
-             if(i >= 0)x = a[i]-'0',i--;
-             else x = 0;
+             if(i >= 0){
+                 x = a[i]-'0';
+                 i--;
+             }
             
-             if(j >= 0)y = b[j]-'0',j--;
-             else y = 0;
+             if(j >= 0){
+                 y = b[j]-'0';
+                 j--;
+             }
             
-            int sum = x + y + carry;
+             sum = x^y^carry;
             
-            carry = sum/2;
-            str += sum%2 + '0';
-            
+             str += sum + '0';
+             
+             carry = (x | y) && (x | carry) && (y | carry);
         }
-        
-        if(carry)str += "1";
         
         reverse(str.begin(),str.end());
         
