f53871c7d7d62b4a9629125f333a4aba4a7093d5
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-14 20:23:52 +0530

    Time: 2 ms (51.75%), Space: 5.9 MB (89.80%) - LeetHub

--- 2162-minimum-cost-to-set-cooking-time/2162-minimum-cost-to-set-cooking-time.cpp
+++ 2162-minimum-cost-to-set-cooking-time/2162-minimum-cost-to-set-cooking-time.cpp
@@ -3,31 +3,16 @@ public:
     
     typedef long long ll;
     
-    ll find(ll min,ll sec,ll startAt,ll moveCost,ll pushCost){
+    ll find(ll mint,ll sec,ll startAt,ll moveCost,ll pushCost){
         
-        string str;
+        if(min(mint,sec) < 0 || max(mint,sec) >= 100)return INT_MAX;
         
-         if(min < 100){
-
-            if(sec/10 == 0){
-                if(min == 0)str = to_string(sec) ;
-                else str = to_string(0) + to_string(sec);
-            }
-            else{
-                str = to_string(sec);
-            }
-            // cout<<str<<endl;
-            if(min > 0){
-                str = to_string(min) + str;
-            }
-         }
-        
+        string str = to_string(mint*100 + sec);
+       
          // cout<<str<<endl;
          ll cost = 0;
         
          for(ll i=0;i<str.size();i++){
-             
-             // cout<<str[i]<<" , "<<startAt<<endl;
              
              if((str[i]-'0') != startAt){
                  cost += moveCost;
@@ -45,20 +30,18 @@ public:
         
         ll s = 60;
         
-        ll min1 = ts/s;
+        ll mint1 = ts/s;
         ll sec1 = ts%s;
         
-        ll min2 = min1-1;
+        ll mint2 = mint1-1;
         ll sec2 = sec1 + s;
         
-         ll x = INT_MAX , y = INT_MAX;
         
-         if(min1<100)x = find(min1,sec1,startAt,moveCost,pushCost);
+        ll x = find(mint1,sec1,startAt,moveCost,pushCost);
+     
+        ll y = find(mint2,sec2,startAt,moveCost,pushCost);
         
-         if(min2 >= 0 && sec2 < 100){
-             y = find(min2,sec2,startAt,moveCost,pushCost);
-         }
+    return min(x,y); 
         
-    return (ll)min(x,y);      
     }
 };
