560c066cfe0396034cc03d85399e0b2d5f4a41a1
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-14 14:51:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.23%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -3,13 +3,7 @@ public:
     
     int minFlips(int a, int b, int c) {
         
-        int x = (a&b);
-        int y =  x&c;
+        return __builtin_popcount(a&b) - __builtin_popcount(a&b&c) + __builtin_popcount(((a|b)^c)); 
         
-        int cnt = __builtin_popcount(x) - __builtin_popcount(y);
-        
-        cnt += __builtin_popcount(((a|b)^c));
-        
-        return cnt;
     }
 };
