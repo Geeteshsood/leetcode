c4eabe72091047626d9cce58e9fa6cbe6f70b6c1
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-21 14:17:38 +0530

    Time: 131 ms (41.20%), Space: 20.3 MB (20.27%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,58 +1,33 @@
 class Solution {
 public:
-    
-    vector<int> res;
-    
-    void find(int i,int sum,vector<int> &nums,int mask,int val){
-      
-        int n = nums.size();
-        
-        if(i == n){
-            if(sum == val)res.push_back(mask);
-            return ;
-        }
-        
-        find(i+1,sum + nums[i],nums,mask|(1<<i),val);
-        find(i+1,sum,nums,mask,val);
-    }
-    
-    bool check(int i,int mask,int cnt,int k,int &target,vector<vector<vector<int>>> &memo){
-        
-        int n = res.size();
-        
-        if(cnt == k){
-            return mask == target;
-        }
-        if(i == n || cnt > k)return false;
-        
-        if(memo[i][cnt][mask] != -1)return memo[i][cnt][mask];
-        
-        bool flag = false;
-        
-        if(!(mask & res[i])){
-            flag = check(i+1,mask^res[i],cnt+1,k,target,memo);
-        }
-        return memo[i][cnt][mask] = flag || check(i+1,mask,cnt,k,target,memo);
-    }
-    
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         
-        int n = nums.size();
+        int n = nums.size() , target = 0 , sum = 0;
         
-        int sum = accumulate(nums.begin(),nums.end(),0);
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+        }
         
-        if(sum%k != 0)return false;
+        if(sum%k == 0){
+            target = sum/k;
+        }
+        else return false;
         
-        int val = sum/k;
+        vector<int> dp(1<<n,-1);
+        dp[0] = 0;
         
-        find(0,0,nums,0,val);
-        
-        int target = (1<<n)-1;
-        
-        vector<vector<vector<int>>> memo(res.size(),vector<vector<int>> (k,vector<int> (target + 1,-1)));
-        
-        if(res.size() >= k)return check(0,0,0,k,target,memo);
-    
-       return false; 
+        for(int mask=0;mask<(1<<n);mask++){
+            
+            if(dp[mask] == -1)continue;
+            
+            for(int i=0;i<n;i++){
+                
+                if( !(mask & (1<<i))  && (dp[mask] + nums[i] <= target)){
+                     dp[mask|(1<<i)] = (dp[mask] + nums[i])%target ;
+                }
+            }
+        }
+     
+        return !dp[(1<<n)-1];
     }
 };
