d71f2bbaa52b1a8c2103c25f06ac6b1a8ccb5788
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-25 15:48:53 +0530

    Time: 12 ms (97.36%), Space: 9 MB (84.30%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -1,51 +1,49 @@
-class Solution{
+class Solution {
 public:
-    
-    int find(int i,vector<vector<int>> &graph,int mask,vector<vector<int>> &dp){
+    int shortestPathLength(vector<vector<int>>& graph) {
         
         int n = graph.size();
         
-        mask = mask|(1<<i);
-
-        if(mask == ((1<<n)-1))return 0;
-            
-        if(dp[i][mask] > 0){
-            return dp[i][mask];
-        }
+        int target = (1<<n)-1;
+        vector<vector<bool>> vis(n,vector<bool> (target+1,false));
         
-        if(dp[i][mask] == -1){
-            dp[i][mask] = 0;
-        }
-        else return INT_MAX;
-        
-        int ans = INT_MAX;
-        
-        for(auto &itr : graph[i]){
-            
-            int val = find(itr,graph,mask,dp);
-            ans = min(ans,val);
-        }
-
-        if(ans == INT_MAX){
-            dp[i][mask] = -1;
-            return ans;
-        }
-        
-        return dp[i][mask] = ans + 1;
-    }
-    
-    int shortestPathLength(vector<vector<int>>& graph) {
-        
-        int n = graph.size() , ans = INT_MAX;
-        // vector<vector<int>> dp(n,vector<int>(1<<n,-1));
+        queue<pair<int,int>> q;
         
         for(int i=0;i<n;i++){
-            vector<vector<int>> dp(n,vector<int>(1<<n,-1));
-           int x = find(i,graph,0,dp);
-           // cout<<x<<endl;
-           ans = min(ans,x);
+            int mask = (1<<i);
+            if(mask == target)return 0;
+            vis[i][mask] = true;
+            q.push({i,mask});
         }
         
-        return ans;
+        int lev = 0;
+        
+        while(q.size()){
+            
+          int size = q.size();
+            
+          for(int i=0;i<size;i++){
+              
+              auto [node,mask] = q.front();
+              q.pop();
+              
+              for(auto &itr : graph[node]){
+                  
+                  int nmask = mask | (1<<itr);
+                  
+                  if(nmask == target)return lev + 1;
+                  
+                  if(!vis[itr][nmask]){
+                      
+                      vis[itr][nmask] = true;
+                      
+                      q.push({itr,nmask});
+                  }
+              }
+          }
+          lev++;  
+        }
+        
+     return lev;   
     }
 };
