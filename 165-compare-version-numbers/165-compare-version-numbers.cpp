91fece28bf0ff518f3e4019fe2d8fa165f33e970
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-02-25 14:55:10 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (95.20%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,69 +1,32 @@
 class Solution {
 public:
+    
     int compareVersion(string s1, string s2) {
         
         int m = s1.size() , n = s2.size();
         
-        vector<int> dp1,dp2;
+        int i = 0 , j = 0;
         
-        for(int i=0;i<m;i++){
+        for(;i<m || j<n;i++,j++){
             
-            int mask = 0;
-            bool flag = true;
+            int lval = 0;
             
             while(i < m && s1[i]!='.'){
-               if(s1[i] == '0' && flag){
-                   i++;
-                   continue;
-               }
-               else flag = false;
-                
-               mask=mask*10 + (s1[i]-'0'); 
+               lval = lval*10 + (s1[i]-'0'); 
                i++;
             }
             
-            dp1.push_back(mask);
-        }
-        
-       for(int i=0;i<n;i++){
-            
-            int mask = 0;
-            bool flag = true;
+            int rval = 0;
            
-            while(i < n && s2[i]!='.'){
-               if(s2[i] == '0' && flag){
-                   i++;
-                   continue;
-               }
-               else flag = false;
-                
-               mask=mask*10 + (s2[i]-'0'); 
-               i++;
+            while(j < n && s2[j]!='.'){
+               rval = rval*10 + (s2[j]-'0'); 
+               j++;
             }
             
-            dp2.push_back(mask);
-        }
-        
-        // for(auto &i: dp1)cout<<i<<" ";
-        // cout<<endl;
-        // for(auto &i: dp2)cout<<i<<" ";
-        // cout<<endl;
-        
-        int i=0,j=0;
-        for(;i<min(dp1.size(),dp2.size());i++,j++){
-            if(dp1[i] != dp2[j]){
-                if(dp1[i] < dp2[j])return -1;
-                else return 1;
-            }
+            if(lval < rval)return -1;
+            else if(lval > rval)return 1;
         }
 
-        for(int x=i;x<dp1.size();x++){
-            if(dp1[x] != 0)return 1;
-        }
-        for(int x=j;x<dp2.size();x++){
-            if(dp2[x] != 0)return -1;
-        }
-       
-        return 0;
+     return 0; 
     }
 };
