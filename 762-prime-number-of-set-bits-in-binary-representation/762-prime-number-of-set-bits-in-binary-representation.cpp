bc301fedde674f1b31146dc1d70fbe95da96daaa
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-05 12:09:56 +0530

    Time: 22 ms (44.20%), Space: 6 MB (47.40%) - LeetHub

--- 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    
-    bool checkprime(int n){
-     return n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13 || n == 17 || n == 19;
-    }
-    
+
     int countPrimeSetBits(int left, int right) {
         
+        unordered_set<int> prime{2,3,5,7,11,13,17,19};
         int cnt = 0;
         
         for(int num=left;num<=right;num++){
             
             int bit = __builtin_popcount(num);
             
-            if(checkprime(bit))cnt++;
+            if(prime.find(bit) != prime.end())cnt++;
             
         }
        
