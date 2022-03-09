0ebf502414ba01fdc25356764a9659f3e627bf55
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-09 16:26:35 +0530

    Time: 696 ms (99.71%), Space: 309.6 MB (20.50%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,53 +1,52 @@
 class Solution {
 public:
-    
-    
-   TreeNode* dfs(int root,vector<int> &left,vector<int> &right){
-        
-        // cout<<root<<endl;
-
-        TreeNode* node = new TreeNode(root);
-        
-        if(left[root]!=-1)node->left = dfs(left[root],left,right);
-        else node->left = NULL;
-       
-        if(right[root]!=-1)node->right = dfs(right[root],left,right);
-        else node->right = NULL;
-       
-        return node;
-    }
-    
     TreeNode* createBinaryTree(vector<vector<int>>& edges) {
         
          int high = INT_MIN;
+         vector<int> vis(1e5+1,false);
+        
          for(auto &it : edges){
              high = max({high,it[0],it[1]});
+             vis[it[1]] = true;
          }
         
-         vector<int> vis(high+1,false);
+         TreeNode* root;
+        
          vector<int> left(high+1,-1);
          vector<int> right(high+1,-1);
         
          for(auto &i : edges){
              int u = i[0] , v = i[1] , wt = i[2];
-             vis[v] = true;
+             if(!vis[u])root = new TreeNode (u);
              
              if(wt == 1)left[u]=v;
              if(wt == 0)right[u]=v;
-             // cout<<"*"<<endl;
          }
-
+         
+        queue<TreeNode*> q;
+        q.push(root);
         
-         for(auto &i : edges){
-             int u = i[0] , v = i[1] , wt = i[2];
-             if(!vis[u]){
-                 return dfs(u,left,right);
-             }
-             if(!vis[v]){
-                 return dfs(v,left,right);
-             }
-         }
-         return NULL;
+        while(q.size()){
+            
+            int size = q.size();
+            
+            for(int i=0;i<size;i++){
+                
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if(left[node->val] != -1){
+                    node->left = new TreeNode (left[node->val]);
+                    q.push(node->left);
+                }
+                if(right[node->val] != -1){
+                    node->right = new TreeNode (right[node->val]);
+                    q.push(node->right);
+                }
+                
+            }
+        }
         
+       return root; 
     }
 };
