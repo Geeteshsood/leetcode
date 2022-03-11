2705a1c7059f5f71f528ae8987dba63dfc4cf259
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-11 17:12:28 +0530

    Time: 6 ms (25.18%), Space: 6.7 MB (31.94%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,19 +1,48 @@
+// class Solution {
+// public:
+//     int combinationSum4(vector<int>& nums, int target) {
+        
+//        long int mod=1000000007;   
+//        vector<int> dp(target+1,0);
+        
+//         dp[0]=1;
+        
+//        for(int j=0;j<=target;j++){  
+//           for(int i=0;i<nums.size();i++){
+//               if(j-nums[i]>=0)
+//                 dp[j]+=dp[j-nums[i]]%mod;
+//             }
+//         }
+        
+// return dp[target];   
+        
+//     }
+// };
+
 class Solution {
 public:
-    int combinationSum4(vector<int>& nums, int target) {
-     long int mod=1000000007;   
-     vector<int> dp(target+1,0);
+    
+    int combinationSum4(vector<int>& nums, int t) {
         
-        dp[0]=1;
+        long int m = 1e9 + 7;
         
-    for(int j=0;j<=target;j++){  
-        for(int i=0;i<nums.size();i++){
-              if(j-nums[i]>=0)
-                dp[j]+=dp[j-nums[i]]%mod;
+        int n = nums.size();
+        vector<int> dp(t+1,0);
+        dp[0] =1;
+        
+        for(int i = 1 ;i<=t ; i++){
+            
+            for(int j = 0 ; j<n ; j++){
+                
+                if(nums[j] <= i){
+                    dp[i] += dp[i - nums[j]]%m;
+                }
+                
             }
+            
         }
         
-return dp[target];   
+        return dp[t];
         
     }
 };
