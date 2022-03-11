227fc25c1400fcf1ed8ac9b427a393a813b8b6dd
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-11 17:24:44 +0530

    Time: 32 ms (44.98%), Space: 6.7 MB (97.49%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
@@ -1,60 +1,33 @@
 class Solution {
 public:
-    
-    int check(int x,int y,string &s){
-        
-       vector<int> first(26,-1),last(26,-1);
-       
-       for(int i=x;i<=y;i++){
-           if(first[s[i]-'a'] == -1)first[s[i]-'a'] = i;
-           last[s[i]-'a'] = i;
-       }
-       
-       int ch = -1 , mini = INT_MAX;
-       
-       for(int i=0;i<26;i++){
-           if(first[i] != -1 && last[i] != -1){
-               int moves = first[i]-x + y - last[i];
-               if(moves < mini){
-                   mini = moves;
-                   ch = i;
-               }
-           }
-       } 
-        
-       for(int i=first[ch];i>x;i--)swap(s[i],s[i-1]);
-       for(int i=last[ch];i<y;i++)swap(s[i],s[i+1]);
-        
-       return mini;
-    }
-    
-    int find(int i,int j,string &s){
-        
-        int n = s.size();
-        
-        if(i > j){
-            return 0;
-        }
-          
-        if(s[i] == s[j]){
-            return find(i+1,j-1,s);
-        }
-        else{
-           
-           int moves = check(i,j,s);
-           
-           if(moves == INT_MAX)return -1;
-            
-           return moves + find(i+1,j-1,s); 
-        }
-        
-        return -1;
-    }
-    
     int minMovesToMakePalindrome(string s) {
         
-        int n = s.size();
-        return find(0,n-1,s);
+        int ans = 0 , n = s.size();
         
+        for(int l=0,r=n-1;l<r;l++,r--){
+            
+            int idx1 = l;
+            
+            while(s[r] != s[idx1])idx1++;
+            
+            int move1 = idx1 - l;
+            
+            int idx2 = r;
+            
+            while(s[idx2] != s[l])idx2--;
+            
+            int move2 = r-idx2;
+            
+            if(move1 < move2){
+                for(int i=idx1;i>l;i--)swap(s[i],s[i-1]);
+            }
+            else{
+                for(int i=idx2;i<r;i++)swap(s[i],s[i+1]);
+            }
+            
+            ans += min(move1,move2);
+        }
+   
+        return ans;
     }
 };
