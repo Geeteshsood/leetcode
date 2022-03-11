c045f4174a2c4db97201c8a8cb5a69534319cb86
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-11 12:52:00 +0530

    Time: 300 ms (5.31%), Space: 20.2 MB (7.99%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.cpp
@@ -1,61 +1,60 @@
 class Solution {
 public:
     
-    int find(string s){
+    int check(int x,int y,string &s){
+        
+       vector<int> first(26,-1),last(26,-1);
+       
+       for(int i=x;i<=y;i++){
+           if(first[s[i]-'a'] == -1)first[s[i]-'a'] = i;
+           last[s[i]-'a'] = i;
+       }
+       
+       int ch = -1 , mini = INT_MAX;
+       
+       for(int i=0;i<26;i++){
+           if(first[i] != -1 && last[i] != -1){
+               int moves = first[i]-x + y - last[i];
+               if(moves < mini){
+                   mini = moves;
+                   ch = i;
+               }
+           }
+       } 
+        
+       for(int i=first[ch];i>x;i--)swap(s[i],s[i-1]);
+       for(int i=last[ch];i<y;i++)swap(s[i],s[i+1]);
+        
+       return mini;
+    }
+    
+    int find(int i,int j,string &s){
         
         int n = s.size();
         
-        // cout<<s<<endl;
-        
-        if(n == 0){
+        if(i > j){
             return 0;
         }
           
-        if(s[0] == s[n-1]){
-            return find(s.substr(1,n-2));
+        if(s[i] == s[j]){
+            return find(i+1,j-1,s);
         }
         else{
+           
+           int moves = check(i,j,s);
+           
+           if(moves == INT_MAX)return -1;
             
-            vector<int> first(26,-1),last(26,-1);
-            
-            for(int i=0;i<n;i++){
-                if(first[s[i]-'a'] == -1)first[s[i]-'a'] = i;
-                last[s[i]-'a'] = i;
-            }
-            
-            int ch = -1 , mini = INT_MAX;
-            
-            for(int i=0;i<26;i++){
-                if(first[i] != -1 && last[i] != -1){
-                    int moves = first[i] + n - last[i] - 1;
-                    if(moves < mini){
-                        mini = moves;
-                        ch = i;
-                    }
-                }
-            }
-            
-            if(ch != -1){
-                // cout<<ch<<endl;
-             string p;
-             if(first[ch] > 0 )p += s.substr(0,first[ch]);
-             if(last[ch]-first[ch] > 1)p += s.substr(first[ch]+1,last[ch]-first[ch] - 1);
-             if(last[ch]+1 < n)p += s.substr(last[ch] + 1);
-                
-             // cout<<p<<endl;
-            
-             return mini + find(p);
-                
-            }
+           return moves + find(i+1,j-1,s); 
         }
         
-        return 0;
+        return -1;
     }
     
     int minMovesToMakePalindrome(string s) {
         
         int n = s.size();
-        return find(s);
+        return find(0,n-1,s);
         
     }
 };
