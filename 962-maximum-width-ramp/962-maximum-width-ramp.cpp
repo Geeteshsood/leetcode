2e521e293a1cd621b9beadb66246733c49be23da
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-14 00:10:06 +0530

    Time: 111 ms (14.07%), Space: 29.4 MB (62.26%) - LeetHub

--- 962-maximum-width-ramp/962-maximum-width-ramp.cpp
+++ 962-maximum-width-ramp/962-maximum-width-ramp.cpp
@@ -1,50 +1,39 @@
 class Solution {
 public:
     
-    int search(int i,vector<int> &v,vector<int> &nums){
+    int search(int i,vector<int> & arr,vector<int> &nums){
         
-        int n = v.size();
-        
-        int start = 0 , end = n-1 , ans = -1;
+        int start = 0 , end = arr.size()-1 , ans = -1;
         
         while(start <= end){
             
             int mid = start + (end-start)/2;
             
-            if(nums[v[mid]] >= nums[i]){
-                // if(nums[i] == 0)cout<<v[mid]<<" ** ";
-                ans = v[mid];
+            if(nums[arr[mid]] >= nums[i]){
+                ans = arr[mid];
                 end = mid - 1;
             }
             else start = mid + 1;
-            
         }
-//         if(nums[i] == 0)cout<<endl;
-//         cout<<nums[i]<<" "<<endl;
-//         for(auto &x : v)cout<<nums[x]<<" ";
-//         cout<<endl;
-//         cout<<ans<<endl;
-        
+
         return ans;
     }
     
     int maxWidthRamp(vector<int>& nums) {
         
         int n = nums.size() , ans = INT_MIN;
-        vector<int> v;
         
-        for(int i=n-1;i>=0;i--){
+        vector<int> arr({n-1});
+        
+        for(int i=n-2;i>=0;i--){
             
-            if(v.size()){
-                int x = search(i,v,nums);
-                if(x != -1)ans = max(ans,x-i);
+            int idx = search(i,arr,nums);
+            
+            if(idx != -1)ans = max(ans,idx-i);
+            
+            if(nums[i] > nums[arr.back()]){
+                arr.push_back(i);
             }
-            
-            if(v.size()){
-                if(nums[i] > nums[v.back()])v.push_back(i);
-            }
-            else v.push_back(i);
-            
         }
         
         return ans==INT_MIN?0:ans;
