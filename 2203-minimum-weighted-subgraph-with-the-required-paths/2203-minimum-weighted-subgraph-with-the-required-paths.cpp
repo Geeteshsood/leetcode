0ee1fbe95536a1bc1b8ad60cf38b23c0da11e817
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-13 20:09:22 +0530

    Time: 887 ms (35.71%), Space: 143.7 MB (57.14%) - LeetHub

--- 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
+++ 2203-minimum-weighted-subgraph-with-the-required-paths/2203-minimum-weighted-subgraph-with-the-required-paths.cpp
@@ -1,68 +1,38 @@
 class Solution {
+    typedef pair<long, long> ipair;
 public:
-    typedef long ll;
-    
-    typedef pair<ll,ll> pi;
-    
-    void djikstra(ll src,vector<vector<pi>> &adj,vector<ll> &dist){
-        
-        ll n = adj.size();
-        
-        dist[src] = 0;
-        
-        priority_queue<pi,vector<pi>,greater<pi>> pq;
-        
-        pq.push({0,src});
-
-        while(pq.size()){
-     
-           ll u = pq.top().second;
-           ll path = pq.top().first;
-           pq.pop();
-            
-           if(dist[u] != path)continue;
-            
-           for(auto &it:adj[u]){
-               
-               ll v =  it.first;
-               ll wt = it.second;
-               
-               if(dist[v] > path + wt){
-                   dist[v] = path + wt;
-                   pq.push({dist[v],v});
-               }
-           }
+    long long minimumWeight(int n, vector<vector<int>>& E, int a, int b, int dest) {
+        vector<vector<ipair>> G(n), R(n); // `G` is the original graph. `R` is the reversed graph
+        for (auto &e : E) {
+            long u = e[0], v = e[1], w = e[2];
+            G[u].emplace_back(v, w);
+            R[v].emplace_back(u, w);
         }
-        
-    }
-    
-    long long minimumWeight(ll n, vector<vector<int>>& edges, ll src1, ll src2, ll dest) {
-        
-        vector<vector<pi>> adj(n) , rev(n);
-        
-        for(auto &it : edges){
-            ll u = (ll)it[0] , v = (ll)it[1] , wt = (ll)it[2];
-            
-            adj[u].push_back({v,wt});
-            rev[v].push_back({u,wt});
-        }
-        
-        vector<ll> dist1(n,LONG_MAX),dist2(n,LONG_MAX),dist3(n,LONG_MAX);
-        ll ans = LONG_MAX;
-        
-        djikstra(src1,adj,dist1);
-        djikstra(src2,adj,dist2);
-        djikstra(dest,rev,dist3);
-    
-
-        for(int i=0;i<n;i++){
-            if(dist1[i] != LONG_MAX && dist2[i] != LONG_MAX && dist3[i] != LONG_MAX){
-                ans = min(ans,dist1[i] + dist2[i] + dist3[i]);
+        vector<long> da(n, LONG_MAX), db(n, LONG_MAX), dd(n, LONG_MAX);
+        auto solve = [&](vector<vector<ipair>> &G, int a, vector<long> &dist) {
+            priority_queue<ipair, vector<ipair>, greater<ipair>> pq;
+            dist[a] = 0;
+            pq.emplace(0, a);
+            while (pq.size()) {
+                auto [cost, u] = pq.top();
+                pq.pop();
+                if (cost > dist[u]) continue;
+                for (auto &[v, c] : G[u]) {
+                    if (dist[v] > dist[u] + c) {
+                        dist[v] = dist[u] + c;
+                        pq.emplace(dist[v], v);
+                    }
+                }
             }
+        };
+        solve(G, a, da);
+        solve(G, b, db);
+        solve(R, dest, dd);
+        long ans = LONG_MAX;
+        for (int i = 0; i < n; ++i) {
+            if (da[i] == LONG_MAX || db[i] == LONG_MAX || dd[i] == LONG_MAX) continue;
+            ans = min(ans, da[i] + db[i] + dd[i]);
         }
-        
-        if(ans == LONG_MAX)ans = -1;
-        
-     return ans;
+        return ans == LONG_MAX ? -1 : ans;
     }
 };
