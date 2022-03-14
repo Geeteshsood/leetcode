0c073a59fe6fc5d455bf4d2b6acf74a7f8c2619e
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-14 09:03:55 +0530

    Time: 64 ms (66.27%), Space: 29.4 MB (61.45%) - LeetHub

--- 962-maximum-width-ramp/962-maximum-width-ramp.cpp
+++ 962-maximum-width-ramp/962-maximum-width-ramp.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-    
-    int search(int i,vector<int> & arr,vector<int> &nums){
-        
-        int start = 0 , end = arr.size()-1 , ans = -1;
-        
-        while(start <= end){
-            
-            int mid = start + (end-start)/2;
-            
-            if(nums[arr[mid]] >= nums[i]){
-                ans = arr[mid];
-                end = mid - 1;
-            }
-            else start = mid + 1;
-        }
-
-        return ans;
-    }
-    
     int maxWidthRamp(vector<int>& nums) {
         
-        int n = nums.size() , ans = INT_MIN;
+        int n = nums.size();
         
-        vector<int> arr({n-1});
+        stack<int> st;
         
-        for(int i=n-2;i>=0;i--){
-            
-            int idx = search(i,arr,nums);
-            
-            if(idx != -1)ans = max(ans,idx-i);
-            
-            if(nums[i] > nums[arr.back()]){
-                arr.push_back(i);
-            }
+        for(int i=n-1;i>=0;i--){
+            if(st.empty() || (nums[i] > nums[st.top()]))st.push(i);
         }
         
-        return ans==INT_MIN?0:ans;
+        int reach = 0;
+        
+        for(int i=0;i<n-reach;i++){
+            
+            while(st.size() && nums[i] <= nums[st.top()]){
+                reach = max(reach, st.top() - i);
+                st.pop();
+            }
+            
+        }
+       
+        return reach;
     }
 };
