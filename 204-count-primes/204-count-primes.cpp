f14d896e2be38b9ae9dc01a4430061058087b41f
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-17 12:46:33 +0530

    Time: 196 ms (81.27%), Space: 10.3 MB (35.29%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -5,7 +5,7 @@ public:
         
         vector<bool> isPrime(n,true);
 
-        for(int i=2;i<sqrt(n);i++){
+        for(int i=2;i*i<n;i++){
             
             if(isPrime[i]){
                 for(int j=i*i;j<n;j+=i){
