4b82b5e6f9dd876467072ef53efe19957d3f20a4
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-18 16:27:37 +0530

    Time: 289 ms (63.36%), Space: 153.3 MB (7.36%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -14,49 +14,50 @@ public:
         ll n = nums.size();
         ll odd = 0;
         
-        stack<ll> st1,st2;
+        vector<ll> st1,st2;
         
         for(ll i=0;i<n;i++){
-            st1.push((ll)nums[i]);
+            st1.push_back((ll)nums[i]);
         }
+        
         ll sz = n;
         
         while(true){
             
             if(st1.size() >= 2){
-                ll x = st1.top();
-                st1.pop();
-                ll y = st1.top();
-                st1.pop();
                 
-                // cout<<x<<" "<<y<<endl;
+                ll x = st1.back();
+                st1.pop_back();
+                ll y = st1.back();
+                st1.pop_back();
+                
                 ll gcd1 = gcd(x,y);
             
                 if(gcd1 > 1){
                     ll lcm = (x*y)/gcd1;
-                    st1.push(lcm);
+                    st1.push_back(lcm);
                 }
-                else st1.push(y),st2.push(x);
+                else st1.push_back(y),st2.push_back(x);
             }
             else if(st1.size() == 1){
                 odd++;
-                st2.push(st1.top());
-                st1.pop();
+                st2.push_back(st1.back());
+                st1.pop_back();
                 if(st2.size() == sz)break;
                 st1 = st2;
                 sz = st2.size();
-                while(st2.size())st2.pop();
+                st2.clear();
             }
             
         }
         
         vector<int> ans;
         
-        while(st2.size()){
-            ans.push_back(st2.top());
-            st2.pop();
+        for(auto &i : st2){
+            ans.push_back(i);
         }
-        if(odd%2 == 0)reverse(ans.begin(),ans.end());
+        
+        if(odd%2 != 0)reverse(ans.begin(),ans.end());
         
         return ans;
     }
