cf7360f8a154279754d4f2962ce99988416e0593
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-20 20:08:48 +0530

    Time: 276 ms (46.07%), Space: 9 MB (50.00%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -1,59 +1,40 @@
 class Solution {
 public:
-
-    vector<int> v;
-    int ans = 0;
-    
-    int calculate(vector<int> v){
-        
-        sort(v.begin(),v.end());
-        
-        int sum = 0;
-        
-        for(int i=0;i<v.size();i++){
-             sum += (i+1)*v[i];
-        }
-        
-        return sum;
-    }
-    
-    void find(int i,vector<int> &nums){
-        
-        int n = nums.size();
-        while(i<n && nums[i] == -1)i++;
-        
-        if(i == n){
-            
-            ans = max(ans,calculate(v));
-            return ;
-            
-        }
-        
-        int val = INT_MIN;
-        
-        for(int j=i+1;j<n;j++){
-            
-            if(nums[j] == -1)continue;
-            
-            int score = __gcd(nums[i],nums[j]);
-            
-            int num = nums[j];
-            nums[j] = -1;
-            
-            v.push_back(score);
-            find(i+1,nums);
-            v.pop_back();
-
-            nums[j] = num;
-        }
-    }
     
     int maxScore(vector<int>& nums) {
         
         int n = nums.size();
-
-        find(0,nums);
         
-        return ans;
+        int target = 1<<n;
+        vector<int> dp(target,INT_MIN);
+        dp[0] = 0;
+        
+        for(int mask=0;mask<target;mask++){
+            
+           int setbits = __builtin_popcount(mask);
+            
+           if(setbits % 2 != 0)continue;
+            
+           if(dp[mask] == INT_MIN)continue;
+        
+           int opt = setbits/2;
+            
+           for(int i=0;i<n;i++){
+               
+               if(mask & (1<<i))continue;
+      
+               for(int j=i+1;j<n;j++){
+                   
+                   if(mask & (1<<j))continue;
+                   
+                   int nmask = (mask | (1<<i)) | (mask | (1<<j));
+                   
+                   dp[nmask] = max(dp[nmask] , (opt+1)*gcd(nums[i],nums[j]) + dp[mask]);
+               }
+           }
+            
+        }
+      
+        return dp[target-1];
     }
 };
