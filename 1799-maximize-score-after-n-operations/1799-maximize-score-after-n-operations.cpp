5080b5904491606f74b5ecfdbab11868863a1a47
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-20 20:18:34 +0530

    Time: 308 ms (37.86%), Space: 8.8 MB (56.07%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -7,17 +7,18 @@ public:
         
         int target = 1<<n;
         vector<int> dp(target,INT_MIN);
+        
         dp[0] = 0;
         
         for(int mask=0;mask<target;mask++){
             
            int setbits = __builtin_popcount(mask);
             
-           if(setbits % 2 != 0)continue;
+           if(setbits % 2 != 0)continue; // odd bits is not possible as we always have to choose 2 numbers
             
-           if(dp[mask] == INT_MIN)continue;
+           if(dp[mask] == INT_MIN)continue;      // if this cannot be achieved
         
-           int opt = setbits/2;
+           int opt = setbits/2;                 // calculating how many operation have been performed
             
            for(int i=0;i<n;i++){
                
@@ -25,9 +26,9 @@ public:
       
                for(int j=i+1;j<n;j++){
                    
-                   if(mask & (1<<j))continue;
+                   if(mask & (1<<j))continue;               
                    
-                   int nmask = (mask | (1<<i)) | (mask | (1<<j));
+                   int nmask = mask | (1<<i) | (1<<j);   // setting bits of 2 chosen numbers (i,j)
                    
                    dp[nmask] = max(dp[nmask] , (opt+1)*gcd(nums[i],nums[j]) + dp[mask]);
                }
