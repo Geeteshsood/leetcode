bcf06f7926e2a70e61ab0c18332bd80c2c0c49a9
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-20 21:29:22 +0530

    Time: 246 ms (59.64%), Space: 7.8 MB (83.21%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     
-    int maxScore(vector<int>& nums) {
+    int dp[1<<14];
+    
+    int find(int mask,int opt,vector<int> &nums){
         
         int n = nums.size();
         
-        int target = 1<<n;
-        vector<int> dp(target,INT_MIN);
+        if(dp[mask] != -1)return dp[mask];
         
-        dp[0] = 0;
+        if(mask == ((1<<n)-1))return 0;
         
-        for(int mask=0;mask<target;mask++){
-            
-           int setbits = __builtin_popcount(mask);
-            
-           if(setbits % 2 != 0)continue; // odd bits is not possible as we always have to choose 2 numbers
-            
-           if(dp[mask] == INT_MIN)continue;      // if this cannot be achieved
-        
-           int opt = setbits/2;                 // calculating how many operation have been performed
-            
-           for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
                
-               if(mask & (1<<i))continue;
-      
-               for(int j=i+1;j<n;j++){
-                   
-                   if(mask & (1<<j))continue;               
-                   
-                   int nmask = mask | (1<<i) | (1<<j);   // setting bits of 2 chosen numbers (i,j)
-                   
-                   dp[nmask] = max(dp[nmask] , (opt+1)*gcd(nums[i],nums[j]) + dp[mask]);
-               }
-           }
-            
+            if(mask & (1<<i))continue;
+
+            for(int j=i+1;j<n;j++){
+                
+                if(mask & (1<<j))continue;               
+                
+                int nmask = mask | (1<<i) | (1<<j);   
+                
+                dp[mask] = max(dp[mask],opt*gcd(nums[i],nums[j]) + find(nmask,opt+1,nums));
+            }
         }
-      
-        return dp[target-1];
+            
+      return dp[mask];
+    }
+    
+    int maxScore(vector<int>& nums) {
+    
+         memset(dp,-1,sizeof(dp));
+        
+         return find(0,1,nums);
     }
 };
