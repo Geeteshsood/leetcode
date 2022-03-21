d26e01ca30df4faa5fd8529e4411fdb92077eb3d
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-21 19:42:23 +0530

    Time: 126 ms (12.50%), Space: 20.6 MB (62.50%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int countCollisions(string s) {
+        
+        int n = s.size() , collide = 0;
+        vector<char> st;
+        
+        for(int i=0;i<n;i++){
+            
+            if(s[i] != 'L'){
+                st.push_back(s[i]);
+            }
+            else{
+                char cur = s[i];
+                
+                while(st.size()){
+                    if(st.back() == 'R' && cur == 'L'){
+                        collide+=2;
+                    }
+                    else if(st.back() == 'S' && cur == 'L'){
+                        collide+=1;
+                    }
+                    cur = 'S';
+                    st.pop_back();
+                    break;
+                }
+                if(cur == 'S')st.push_back(cur);
+            }
+            
+        }
+        int cnt = 0;
+        n = st.size();
+        for(int i=n-1;i>=0;i--){
+            if(st[i] == 'S')cnt++;
+            else if(st[i] == 'R' && cnt > 0)collide++;
+        }
+        
+      
+        return collide;
+    }
+};
