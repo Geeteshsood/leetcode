69a33e689af716247099342c94074d50bbd0bd38
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-21 21:44:07 +0530

    Time: 52 ms (100.00%), Space: 16.3 MB (75.00%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -2,42 +2,19 @@ class Solution {
 public:
     int countCollisions(string dir) {
         
-        int n = dir.size() , collide = 0 , cnt = 0 , prev = -1;
+       int n = dir.size();
+       int left = 0 , right = n-1;
         
-        for(int i=0;i<n;i++){
-            
-            if(dir[i] == 'L'){
-                if(i == 0){
-                    prev = 0;
-                    continue;
-                }
-                
-                if(dir[i-1] != 'R' && cnt > 0){
-                    collide++;
-                }
-                else if(dir[i-1] == 'R'){
-                    collide += 2;
-                }
-                
-                 int j = i-2;
-                 while(j >= 0 && j > prev && dir[j] == 'R'){
-                     collide++;
-                     j--;
-                 }
-            }
-            else{
-                int j = i-1;
-                if(i > 0 && dir[i] == 'S'){
-                    while(j>=0 && dir[j] == 'R'){
-                        collide++;
-                        j--;
-                    }
-                }
-                cnt++;
-            }
-            
-            if(dir[i] != 'R')prev = i;
-        }
+       while(left<n && dir[left] == 'L')left++;
+       while(right>=0 && dir[right] == 'R')right--;
+        
+       int collide = 0;
+        
+       for(int i=left;i<=right;i++){
+           if(dir[i] != 'S'){
+               collide++;
+           }
+       }
         
      return collide;   
     }
