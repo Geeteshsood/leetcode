9e2dd377d16c9533fd26e45ca90c63aefc78d2f5
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-22 10:42:21 +0530

    Time: 285 ms (84.75%), Space: 66.7 MB (53.66%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -5,23 +5,23 @@ public:
         
         int m = floor.size();
         
-        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        vector<vector<int>> dp(2,vector<int>(m+1));
         
-        for(int i=0;i<n+1;i++){
+        for(int j=1;j<=m;j++){
+            dp[0][j] = (floor[j-1]-'0') + dp[0][j-1];
+        }
+        
+        for(int i=1;i<n+1;i++){
             for(int j=1;j<=m;j++){
-                if(i == 0){
-                    dp[i][j] = (floor[j-1]-'0') + dp[i][j-1];
-                    continue;
-                }
-                
-                if(j-len>=0){
-                    dp[i][j] = min((floor[j-1]-'0') + dp[i][j-1],dp[i-1][j-len]);
-                }
 
+                if(j-len>=0){
+                    dp[i%2][j] = min((floor[j-1]-'0') + dp[i%2][j-1],dp[(i+1)%2][j-len]);
+                }
+                else dp[i%2][j] = 0;
             }
         }
 
-     return dp[n][m];
+     return dp[n%2][m];
 
     }
 };
