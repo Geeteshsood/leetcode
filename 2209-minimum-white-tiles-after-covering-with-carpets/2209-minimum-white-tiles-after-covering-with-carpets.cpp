ca28153688d69a12eb966ad56bd4635abe36b103
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-22 10:51:27 +0530

    Time: 141 ms (95.28%), Space: 21.5 MB (96.31%) - LeetHub

--- 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     
+    int dp[2][1001];
+    
     int minimumWhiteTiles(string floor, int n, int len) {
         
+        memset(dp,0,sizeof(dp));
+        
         int m = floor.size();
-        
-        vector<vector<int>> dp(2,vector<int>(m+1));
-        
-        for(int j=1;j<=m;j++){
-            dp[0][j] = (floor[j-1]-'0') + dp[0][j-1];
-        }
-        
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<=m;j++){
 
+        for(int i=0;i<n+1;i++){
+            for(int j=1;j<=m;j++){
+                
+                if(i == 0){
+                    dp[i][j] = (floor[j-1]-'0') + dp[i][j-1];
+                    continue;
+                }
+                
                 if(j-len>=0){
                     dp[i%2][j] = min((floor[j-1]-'0') + dp[i%2][j-1],dp[(i+1)%2][j-len]);
                 }
