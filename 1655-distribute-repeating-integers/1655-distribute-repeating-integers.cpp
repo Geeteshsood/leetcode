37c7a4aa5eb936f68fbc47e9e2ac47d993fdf8a5
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-03-29 22:36:16 +0530

    Time: 1363 ms (7.72%), Space: 76.1 MB (26.85%) - LeetHub

--- 1655-distribute-repeating-integers/1655-distribute-repeating-integers.cpp
+++ 1655-distribute-repeating-integers/1655-distribute-repeating-integers.cpp
@@ -34,6 +34,7 @@ public:
                 dp[i][mask] = mem[i][mask];
             }
         }
+          
         if(mem[i][(1<<m)-1])return true;
       }
 
@@ -41,12 +42,7 @@ public:
             for(int mask=0;mask<(1<<m);mask++){
                 dp[i][mask] = mem[i][mask];
                 for(int submask=mask;submask;submask = (submask-1) & mask){
-                    
-                    int nmask = submask^mask;
-                    
-                    if(nmask)dp[i][mask] = dp[i][mask] | (mem[i][nmask] && dp[i-1][submask]);
-                    else dp[i][mask] = dp[i][mask] | dp[i-1][submask];
-
+                    dp[i][mask] = dp[i][mask] | (mem[i][mask^submask] & dp[i-1][submask]);
                 }
             }
             if(dp[i][(1<<m)-1])return true;
