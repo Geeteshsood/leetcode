933d874938670eed0b8cc780c0ecdf4325d1751e
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-04-01 11:55:19 +0530

    Time: 30 ms (50.00%), Space: 8.4 MB (78.96%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    typedef pair<int,int> pi;
+    pi dp[101][101][2];
+    
+    pi find(int i,int M,vector<int> &piles,bool player1){
+        
+        int stone = 0 , maxi = 0 , sec = 0 , n = piles.size();
+        
+        if(i >= n)return {0,0};
+        
+        if(dp[i][M][player1].first != -1)return dp[i][M][player1];
+            
+        for(int X=1;X<=2*M && i+X-1<n;X++){
+            
+            stone += piles[i+X-1];
+            
+            auto it = find(i+X,max(M,X),piles,!player1);
+            
+            if(player1 && stone + it.first > maxi){
+                maxi = stone + it.first;
+                sec = it.second;
+            }
+            else if(!player1 && stone + it.second > sec){
+                sec = stone + it.second;
+                maxi = it.first;
+            }
+        }
+      
+        return dp[i][M][player1] = {maxi,sec};
+    }
+    
+    int stoneGameII(vector<int>& piles) {
+        
+        int n = piles.size();
+        memset(dp,-1,sizeof(dp));
+        
+        pi it = find(0,1,piles,true);
+        
+        return it.first;
+    }
+};
