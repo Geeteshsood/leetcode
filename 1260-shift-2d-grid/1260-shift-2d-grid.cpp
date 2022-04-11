d32f5f628683d63fa992f16bdb75ecfd5ff69509
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-04-11 23:36:15 +0530

    Time: 49 ms (45.01%), Space: 14 MB (40.46%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -3,22 +3,46 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         
         int m = grid.size() , n = grid[0].size();
+        k = k%(m*n);
         
-         for(int shift=0;shift<k;shift++){
-             int val = grid[m-1][n-1];
-             
-             for(int i=m-1;i>=0;i--){
-                 for(int j=n-1;j>=0;j--){
-                     if(j == 0){
-                         if(i!=0)grid[i][j] = grid[i-1][n-1];
-                     }
-                     else grid[i][j] = grid[i][j-1];
-                 }
-             }
-             grid[0][0] = val;
-         }
+        if(k == 0)return grid;
         
-    return grid;
+        vector<vector<int>> mat(m,vector<int> (n));
         
+        int i = m-1 , j = n-1;
+        int r = m-(k/n)-1;
+        int c = n - k%n-1;
+        
+        while(true){
+            
+            if(j < 0)j = n-1,i--;
+            if(c < 0){
+                c = n-1;
+                if(r == 0)break;
+                else r--;
+            }
+            
+            mat[i][j] = grid[r][c];
+            
+            c--;
+            j--;
+            
+        }
+        
+        // cout<<i<<" "<<j<<endl;
+        
+        for(int r=m-1;r>=0;r--){
+            for(int c=n-1;c>=0;c--){
+                mat[i][j] = grid[r][c];
+                j--;
+                if(j < 0){
+                    j = n-1;
+                    if(i == 0)return mat;
+                    else i--;
+                }
+            }
+        }
+        
+        return mat;
     }
 };
