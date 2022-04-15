acd9b4d4789a3bc62979e3b91dea34dbe1c612d1
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-04-15 12:14:12 +0530

    Time: 164 ms (5.22%), Space: 17.4 MB (6.60%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string str, int k) {
+        
+        int n = str.size();
+        
+        vector<int> tr(n),fl(n);
+        
+        if(str[0] == 'T')tr[0] = 1;
+        else fl[0] = 1;
+            
+        for(int i=1;i<n;i++){
+            if(str[i] == 'T'){
+                tr[i] = tr[i-1] + 1;
+                fl[i] = fl[i-1];
+            }
+            else{
+                fl[i] = fl[i-1] + 1;
+                tr[i] = tr[i-1];
+            }
+        }
+        
+        int len = 1 , t = 0 , f = 0;
+        
+        for(int i=0;i<n;i++){
+            
+            if(i != 0) t = tr[i-1] , f = fl[i-1];
+            
+            int start = i , end = n-1 , ans = i;
+            
+            while(start <= end){
+                
+               int mid = start + (end - start)/2;
+                   
+               int x = tr[mid] - t;
+               int y = fl[mid] - f;
+                
+               if(x <= k || y <= k){
+                   ans = mid;
+                   start = mid+1;
+               }
+               else end = mid - 1;
+            }
+            
+            len = max(len , ans - i + 1);
+        }
+        
+      return len;  
+    }
+};
