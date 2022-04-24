48e60e188ae216e425771f9e644dee70262d1303
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-04-24 16:39:28 +0530

    Time: 41 ms (8.56%), Space: 25.2 MB (15.20%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.cpp
@@ -22,24 +22,21 @@ public:
           for(int i=0;i<size;i++){
               
               TreeNode* node = q.front();
-              q.pop();
               
               if(lev == depth-1){
                   
-                  TreeNode* temp1 = new TreeNode(val);
-                  TreeNode* temp2 = new TreeNode(val);
-                  
-                  temp1->left = node->left;
-                  temp2->right = node->right;
-                  
-                  node->left = temp1;
-                  node->right = temp2;
+                  q.front()->left = new TreeNode(val , q.front()->left,NULL);
+                  q.front()->right = new TreeNode(val , NULL , q.front()->right);
                   
               }
               else{
+                  
                 if(node->left)q.push(node->left);
                 if(node->right)q.push(node->right);
+                  
               }
+              
+              q.pop();
           }
             
           lev++;   
