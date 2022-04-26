0736eb7ea5aae204c87ec72cbf4e7211938c288b
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-04-26 23:40:32 +0530

    Time: 23 ms (82.42%), Space: 25.3 MB (62.73%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -2,10 +2,7 @@
 class Solution {
 public:
     
-    vector<TreeNode*> ans;
-    unordered_set<int> s;
-
-    bool find(TreeNode* root,TreeNode* par){
+    bool find(TreeNode* root,TreeNode* par,vector<TreeNode*> &ans,unordered_set<int> &s){
        
         if(root == NULL)return false;       
         
@@ -13,14 +10,14 @@ public:
         
         if(s.find(root->val) != s.end()){
             
-            x = find(root->left,root->left);
-            y = find(root->right,root->right);
+            x = find(root->left,root->left,ans,s);
+            y = find(root->right,root->right,ans,s);
             
         }
         else{
             
-            x = find(root->left,par);
-            y = find(root->right,par);
+            x = find(root->left,par,ans,s);
+            y = find(root->right,par,ans,s);
             
             if(par == root){
               ans.push_back(root);
@@ -35,13 +32,16 @@ public:
     
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
         
+        vector<TreeNode*> ans;
+        unordered_set<int> s;
+        
         for(auto &x : to_delete){
             s.insert(x);
         }
         
         TreeNode* node = new TreeNode(-1);
         
-        find(root,root);
+        find(root,root,ans,s);
        
         return ans;
     }
