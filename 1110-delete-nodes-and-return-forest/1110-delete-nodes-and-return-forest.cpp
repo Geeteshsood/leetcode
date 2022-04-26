f9563a59f34723376cde59350747f627e7a3205e
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-04-26 23:39:09 +0530

    Time: 50 ms (16.24%), Space: 25.4 MB (45.21%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -4,43 +4,33 @@ public:
     
     vector<TreeNode*> ans;
     unordered_set<int> s;
-    unordered_map<TreeNode*,TreeNode*> mp;
-    
-    void dfs(TreeNode* root , TreeNode* par){
-        
-       if(root == NULL)return ;
-        
-       dfs(root->left ,root);
-       dfs(root->right,root);
-        
-       mp[root] = par;
-    }
-    
-    void find(TreeNode* root,TreeNode* par,bool isLeft){
+
+    bool find(TreeNode* root,TreeNode* par){
        
-        if(root == NULL)return ;       
+        if(root == NULL)return false;       
+        
+        bool x = false , y = false;
         
         if(s.find(root->val) != s.end()){
             
-            if(mp[root]->val != -1){
-                if(isLeft)mp[root]->left = NULL;
-                else mp[root]->right = NULL;
-            }
-            
-            find(root->left,root->left,true);
-            find(root->right,root->right,false);
+            x = find(root->left,root->left);
+            y = find(root->right,root->right);
             
         }
-        else {
+        else{
             
-            find(root->left,par,true);
-            find(root->right,par,false);
+            x = find(root->left,par);
+            y = find(root->right,par);
             
             if(par == root){
               ans.push_back(root);
             }
         } 
-         
+      
+        if(x)root->left = NULL;
+        if(y)root->right = NULL;
+        
+        return s.count(root->val);
     }
     
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
@@ -51,8 +41,7 @@ public:
         
         TreeNode* node = new TreeNode(-1);
         
-        dfs(root,node);
-        find(root,root,false);
+        find(root,root);
        
         return ans;
     }
