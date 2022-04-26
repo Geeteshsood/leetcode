f25f579aee0acfc6340502146417c4d73bcc01d5
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-04-26 17:06:23 +0530

    Time: 56 ms (46.55%), Space: 9.4 MB (49.31%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     
-    int dp[22][4002];
-    
     int findTargetSumWays(vector<int>& nums, int target) {
         
         int n = nums.size();
@@ -10,19 +8,20 @@ public:
         
         int mini = -sum , maxi = sum;
         
+        int dp[22][4*sum+1];
         memset(dp,0,sizeof(dp));
         
-        dp[0][2000] = 1;
+        dp[0][2*sum] = 1;
         
         for(int i=1;i<=n;i++){
             
            for(int j=mini;j<=maxi;j++){
                
-              dp[i][j+2000] = dp[i-1][j-nums[i-1]+2000] + dp[i-1][j+nums[i-1]+2000];
+              dp[i][j+2*sum] = dp[i-1][j-nums[i-1]+2*sum] + dp[i-1][j+nums[i-1]+2*sum];
                
            }
         }
         
-        return dp[n][target+2000];
+        return dp[n][target+2*sum];
     }
 };
