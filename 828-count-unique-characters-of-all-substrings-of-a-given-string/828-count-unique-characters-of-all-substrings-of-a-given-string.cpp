aa1612f10064f83b00203c110872389ba15a9e9c
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-05-18 18:09:43 +0530

    Time: 64 ms (59.89%), Space: 15.9 MB (48.18%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    typedef pair<int,int> pi;
-    
     int uniqueLetterString(string s) {
         
         int n = s.size() , sum = 0;
-        vector<pi> pos(26,{-1,-1});
+        
+        int pos[26][2];
+        memset(pos,-1,sizeof(pos));
         
         vector<int> dp(n);
         
         for(int i=0;i<n;i++){
             
-            int j = pos[s[i]-'A'].first , k = pos[s[i]-'A'].second;
+            int j = pos[s[i]-'A'][0] , k = pos[s[i]-'A'][1];
             
             if(i!=0){
                 dp[i] = dp[i-1] + (i-j)-(j-k);
             }
             else dp[i] = 1;
             
-            pos[s[i]-'A'].second = pos[s[i]-'A'].first;
-            pos[s[i]-'A'].first = i;
+            pos[s[i]-'A'][1] = pos[s[i]-'A'][0];
+            pos[s[i]-'A'][0] = i;
             
             sum += dp[i];
         }
