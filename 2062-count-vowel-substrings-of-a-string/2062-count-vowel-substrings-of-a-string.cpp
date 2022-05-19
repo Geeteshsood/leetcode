e86ae75362ee315d802734f1c4e5ef00b6ef19ae
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-05-19 17:17:00 +0530

    Time: 10 ms (55.02%), Space: 6.3 MB (70.88%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -2,44 +2,48 @@ class Solution {
 public:
     
     unordered_map<char,int> mp;
-    
+
     bool isVowel(char ch){
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
-    }
-    
-    bool check(string &str){
-        
-        int n = str.size();
-        vector<bool> res(5,false);
-     
-        
-        for(int i=0;i<n;i++){
-            if(isVowel(str[i])){
-                res[mp[str[i]]] = true;
-            }
-            else return false;
-        }
-        
-       return res[0] && res[1] && res[2] && res[3] && res[4];
     }
     
     int countVowelSubstrings(string s) {
         
         int n = s.size() , cnt = 0;
         
+        vector<int> res(5,0),temp(5,0),set(5,0);
+        
         mp['a'] = 0 , mp['e'] = 1 , mp['i'] = 2 , mp['o'] = 3 , mp['u'] = 4;
         
-        for(int i=0;i<n;i++){
-            string str;
-            for(int j=i;j<n;j++){
-                str += s[j];
-                
-                bool flag = check(str);
-                
-                if(flag){
-                    cnt++;
-                }
-            }
+        int j = 0 , k =0;
+        
+        for(int i=0;i<n && j<n;i++){
+            
+           if(isVowel(s[i])){
+               
+               res[mp[s[i]]]++;
+               
+               temp = res;
+               
+               while(k < i && k < n){
+                   
+                   if(temp[0] && temp[1] && temp[2] && temp[3] && temp[4]){
+                       // cout<<k<<" "<<i<<endl;
+                       cnt++;
+                   }
+                   else break;
+                   
+                   temp[mp[s[k]]]--;
+                   k++;
+               }
+               
+               k = j;
+           }
+           else{
+               j = i+1;
+               res = set;
+           }
+
         }
         
      return cnt;   
