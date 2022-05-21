412fc58a9346a5815d59443445c6b15f9867e13a
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-05-21 11:37:36 +0530

    Time: 14 ms (29.90%), Space: 14.8 MB (44.01%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -3,56 +3,29 @@ public:
     
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
-           ListNode* head = NULL;
+        ListNode* head = NULL;
         
-           if(list1 && list2){
-               if(list1->val < list2->val){
-                   head = list1;
-                   list1 = list1->next;
-               }
-               else{
-                   head = list2;
-                   list2 = list2->next;
-               }
-           }
-           else if(list1){
-               head = list1;
-               list1 = list1->next;
-           }
-           else if(list2){
-               head = list2;
-               list2 = list2->next;
-           }
-           else return head;
+        if(list1 && list2){
+            if(list1->val > list2->val)swap(list1,list2);
+            head = list1;
+        }
+        else if(list1)return list1;
+        else if(list2)return list2;
+        else return NULL;
         
-           ListNode* node = head;
+        while(list1 && list2){
+            
+            ListNode* temp = NULL;
+            
+            while(list1 && list1->val <= list2->val){
+                temp = list1;
+                list1 = list1->next;
+            }
+            
+            temp->next = list2;
+            swap(list1,list2);
+        }
         
-           while(list1 && list2){
-               
-                 if(list1->val > list2->val){
-                       head->next = list2;
-                       list2 = list2->next;
-                 }
-                 else{
-                        head->next = list1;
-                        list1 = list1->next;
-                 }
-
-                 head = head->next;
-           }
-        
-           while(list1){
-               head->next = list1;
-               list1 = list1->next;
-               head = head->next;
-           }
-        
-           while(list2){
-               head->next = list2;
-               list2 = list2->next;
-               head = head->next;
-           }
-        
-        return node;
+        return head;
     }
 };
