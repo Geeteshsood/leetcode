b287098749fa23ecfce64dcb75c2a9c87277381d
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-05-23 21:08:33 +0530

    Time: 4 ms (35.21%), Space: 5.9 MB (100.00%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.cpp
@@ -1,54 +1,60 @@
 class Solution {
 public:
-    
-    void insert(string &s,vector<string> &res){
-        
-        string str;
-        
-        for(auto &ch : s){
-            if(ch == ' '){
-                res.push_back(str);
-                str = "";
-            }
-            else str += ch;
-        }
-        
-        res.push_back(str);
-    }
-    
     bool areSentencesSimilar(string s1, string s2) {
         
         if(s1.size() < s2.size())swap(s1,s2);
         
-        vector<string> res1;
-        vector<string> res2;
+        int m = s1.size() , n = s2.size();
         
-        insert(s1,res1);
-        insert(s2,res2);
+        int l1 = 0 , cnt = 0;
         
-        int n = res1.size() , m = res2.size();
-        
-        if(m > n)return false;
-        
-        for(int i=-1;i<m;i++){
+        for(int i=0;i<m;i++){
             
-            if(i >= 0 && res1[i] != res2[i])return false;
-            
-            bool flag = true;
-            
-            for(int j=m-1,k=n-1;j>i;j--,k--){
-                
-                if(res1[k] != res2[j]){
-                    flag = false;
-                    break;
+            if(s1[i] == s2[i]){
+                if(i == n-1){
+                    if(i < m-1){
+                        if(s1[i+1] == ' ')return true;
+                    }
+                    else return true;
                 }
-                
+                else{
+                   cnt++;
+                   if(s1[i] == ' '){
+                       l1 += cnt;
+                       cnt = 0;
+                   }
+                }
             }
-            
-            if(flag)return true;
-            
+            else break;
+        }
+
+        // cout<<l1<<endl;
+        int j = n-1;
+        cnt = 0;
+        
+        for(int i=m-1;i>=0 && j>=0;i--,j--){
+        
+            if(s1[i] == s2[j]){
+                if(j == 0){
+                    if(i > 0){
+                        if(s1[i-1] == ' '){
+                            return true;
+                        }
+                    }
+                    else return true;
+                }
+                else{
+                  cnt++;
+                  if(s1[i] == ' '){
+                      l1 += cnt;
+                      cnt = 0;
+                  }
+                }
+            }
+            else break;
         }
         
-       return false; 
+        // cout<<l1<<" "<<n<<endl;
+        return l1 >= n;
     }
 };
