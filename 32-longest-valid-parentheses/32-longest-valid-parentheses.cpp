ed722bc73a862b9268bc7939e1a09465e9980590
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-05-24 09:50:11 +0530

    Time: 20 ms (8.03%), Space: 7.5 MB (16.49%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        int n = s.size();
+        
+        stack<int> st;
+        
+        for(int i=0;i<n;i++){
+            // cout<<i<<" "<<st.size()<<endl;
+            if(s[i] == '('){
+                st.push(s[i]);
+                continue;
+            }
+            
+            int sum = 0;
+            bool flag = true;
+            
+            while(st.size() && st.top() != ')' ){
+                if(st.top() == '('){
+                     flag = false;
+                     st.pop();
+                     sum += 2;
+                     st.push(sum + '0'); 
+                     break;
+                }
+                else{
+                    sum += st.top()-'0';
+                    st.pop();
+                }
+            }
+            
+            if(flag){
+                if(sum)st.push(sum + '0');
+                st.push(')');
+            }
+            
+             // cout<<i<<" "<<st.size()<<endl;
+        }
+        // cout<<st.size()<<endl;
+        int sum = 0 , ans = 0;
+        
+        while(st.size()){
+    
+            if(st.top() == ')' || st.top() == '('){
+                ans = max(ans,sum);
+                sum = 0;
+            }
+            else {
+                cout<<st.top()-'0'<<endl;
+                sum += st.top()-'0';
+            }
+            
+            st.pop();
+        }
+        
+        return max(sum,ans);
+    }
+};
