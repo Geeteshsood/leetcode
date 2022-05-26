a8d004805c49b8252f29d95610e37c86f24cfdc4
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-05-26 19:37:41 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (80.34%) - LeetHub

--- 949-largest-time-for-given-digits/949-largest-time-for-given-digits.cpp
+++ 949-largest-time-for-given-digits/949-largest-time-for-given-digits.cpp
@@ -2,40 +2,14 @@ class Solution {
 public:
     string largestTimeFromDigits(vector<int>& arr) {
         
-        int ans = -1;
-        sort(arr.begin(),arr.end());
-        
-        for(int i=0;i<23;i++){
-          int a = arr[0] , b = arr[1] , c = arr[2] , d = arr[3];
-        
-          int val = 1000*a + 100*b + 10*c + d;
-         
-          if(val >= 0 && val<= 2359 && c < 6){
-              ans = max(ans,val);
-          }
-          next_permutation(arr.begin(),arr.end());
+        sort(arr.begin(),arr.end(),greater<int>());
+     
+        do{
+          if((arr[0] < 2 || (arr[0] == 2 && arr[1] < 4 )) && arr[2] < 6)
+            return to_string(arr[0]) + to_string(arr[1]) + ":" + to_string(arr[2]) + to_string(arr[3]);
         }
-        
-        if(ans == 0)return "00:00";
-        
-        if(ans == -1)return "";
-        
-        string str;
-        
-        int val = ans;
-        
-        while(ans){
-            int x = ans%10;
-            str += (x+'0');
-            ans = ans/10;
-        }
-         
-         reverse(str.begin(),str.end());
-        
-         if(val < 1000){
-             str = '0' + str;
-         }
-        
-         return str.substr(0,2) + ":" + str.substr(2);
+        while(prev_permutation(arr.begin(),arr.end()));
+      
+      return "";
     }
 };
