ccd9c0ce983236be684138d0fa2a8872ac758931
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-05-26 16:43:37 +0530

    Time: 14 ms (32.45%), Space: 8.7 MB (16.50%) - LeetHub

---
+++ 482-license-key-formatting/482-license-key-formatting.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        
+        int n = s.size() , cnt = 0;
+        
+        string str,ans;
+        
+        for(int i=n-1;i>=0;i--){
+            
+            if(s[i] == '-')continue;
+            
+            char ch;
+            
+            if(s[i] >= 'a' && s[i] <= 'z'){
+                ch = toupper(s[i]);
+            }
+            else ch = s[i];
+            
+            str += ch;
+            cnt++;
+            
+            if(cnt == k){
+                str += '-';
+                ans += str;
+                cnt = 0;
+                str = "";
+            }
+        }
+        
+        if(str.size())ans += str;
+        
+        reverse(ans.begin(),ans.end());
+        
+        if(ans[0] == '-')return ans.substr(1);
+        
+        return ans;
+    }
+};
