505391a368ab0cd6b42a7e28f120dd54302d40c1
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-05-31 20:22:06 +0530

    Time: 166 ms (9.34%), Space: 7.5 MB (48.16%) - LeetHub

--- 457-circular-array-loop/457-circular-array-loop.cpp
+++ 457-circular-array-loop/457-circular-array-loop.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
     
-    int find(int i,vector<int> &nums,vector<bool> &vis){
-    
-        if(vis[i])return true;
+    int next(int i,vector<int> &nums){
         
         int n = nums.size();
         
-        int idx = ((i + nums[i])%n + n)%n;
-     
-        if(idx == i)return false;
-        else if(nums[i] > 0 && nums[idx] < 0)return false;
-        else if(nums[i] < 0 && nums[idx] > 0)return false;
-        
-       vis[i] = true;
-        
-       if(find(idx,nums,vis))return true;
-        
-       vis[i] = false;
-        
-       return false;
+        return ((i + nums[i])%n + n)%n;
     }
     
     bool circularArrayLoop(vector<int>& nums) {
         
         int n = nums.size();
-        vector<bool> vis(n,false);
         
         for(int i=0;i<n;i++){
-            if(find(i,nums,vis))return true;
+            
+            int slow = i , fast = i;
+            
+        while(nums[slow]*nums[next(fast,nums)] > 0 && nums[fast]*nums[next(next(fast,nums),nums)] > 0){
+                
+                slow = next(slow,nums);
+                fast = next(next(fast,nums),nums);
+                
+                if(slow == fast){
+                    if(abs(nums[slow])%n != 0)return true;
+                    else break;
+                }
+                
+            }
+            
+            int j = next(i,nums);
+            nums[i] = 0;
+            
+            while(nums[j]*nums[i] > 0){
+                nums[j] = 0;
+                i = j;
+                j = next(j,nums);
+            }
+            
         }
         
         return false;
