97248285d8cc411e9619f341ca8b1bb257d7c10c
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-06-02 18:26:09 +0530

    Time: 357 ms (65.03%), Space: 134.7 MB (44.13%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    typedef pair<int,int> pi;
     
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
           int n = nums.size();
-          deque<pi> dq;
+          deque<int> dq;
           vector<int> ans;
         
           for(int i=0;i<n;i++){
               
-              while(dq.size() && nums[i] > dq.back().first){
+              while(dq.size() && nums[i] >= nums[dq.back()]){
                   dq.pop_back();
               }
               
-              while(dq.size() && i-k >= dq.front().second){
+              while(dq.size() && i-k >= dq.front()){
                   dq.pop_front();
               }
               
-              dq.push_back({nums[i],i});
+              dq.push_back(i);
               
-              if(i >= k-1)ans.push_back(dq.front().first);
+              if(i >= k-1)ans.push_back(nums[dq.front()]);
           }
         
         return ans;
