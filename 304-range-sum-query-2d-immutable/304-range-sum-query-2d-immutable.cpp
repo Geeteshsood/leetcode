cbe33cf1cfb48bf769de5753bb313cff2ff88f39
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-06-03 10:27:46 +0530

    Time: 712 ms (29.80%), Space: 148.1 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -18,12 +18,12 @@ public:
             }
         }
         
-         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                cout<<mat[i][j]<<" ";
-            }
-            cout<<endl;
-        }
+        //  for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<mat[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         
     }
     
