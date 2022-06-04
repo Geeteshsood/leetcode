a20c06b96a5913abce6d67fa0fd3dd5926f5b62e
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-06-04 12:07:30 +0530

    Time: 312 ms (26.95%), Space: 27.9 MB (37.39%) - LeetHub

--- 956-tallest-billboard/956-tallest-billboard.cpp
+++ 956-tallest-billboard/956-tallest-billboard.cpp
@@ -6,34 +6,35 @@ public:
         int n = rods.size();
         
         int sum = accumulate(rods.begin(),rods.end(),0);
+        
+        vector<vector<int>> dp(n+1,vector<int> (sum+1,INT_MIN));
+        
         sum = sum/2;
         
-        vector<vector<int>> dp(n+1,vector<int> (5001,INT_MIN));
-        
-        dp[0][2500] = 0;
+        dp[0][sum] = 0;
         
         for(int i=1;i<=n;i++){
             for(int j=-sum;j<=sum;j++){
                 
-                dp[i][2500 + j] = max(dp[i][2500 + j],dp[i-1][2500 + j]);
+                dp[i][sum + j] = max(dp[i][sum + j],dp[i-1][sum + j]);
                 
-                if(j + rods[i-1] <= sum && dp[i-1][2500 + j+rods[i-1]] != INT_MIN){
-                    dp[i][2500 + j] = max(dp[i][2500 + j],dp[i-1][2500 + j+rods[i-1]]);
+                if(j + rods[i-1] <= sum && dp[i-1][sum + j+rods[i-1]] != INT_MIN){
+                    dp[i][sum + j] = max(dp[i][sum + j],dp[i-1][sum + j+rods[i-1]]);
                 }
-                if(j - rods[i-1] >= -sum && dp[i-1][2500 + j-rods[i-1]] != INT_MIN){
-                    dp[i][2500 + j] = max(dp[i][2500 + j],dp[i-1][2500 + j-rods[i-1]] + rods[i-1]);
+                if(j - rods[i-1] >= -sum && dp[i-1][sum + j-rods[i-1]] != INT_MIN){
+                    dp[i][sum + j] = max(dp[i][sum + j],dp[i-1][sum + j-rods[i-1]] + rods[i-1]);
                 }
             }
         }
         
 //         for(int i=0;i<=n;i++){
 //             for(int j=-sum;j<=sum;j++){
-//                 if(dp[i][j+2500] == INT_MIN)cout<<"*"<<" ";
-//                 else cout<<dp[i][j+2500]<<" ";
+//                 if(dp[i][j+sum] == INT_MIN)cout<<"*"<<" ";
+//                 else cout<<dp[i][j+sum]<<" ";
 //             }
 //             cout<<endl;
 //         }
         
-        return dp[n][2500];
+        return dp[n][sum];
     }
 };
