2874626ae13ecc8a7b0ff9e7216c82be3c23dafa
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-06-06 18:33:56 +0530

    Added solution - LeetHub

--- Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
+++ Maximize The Cut Segments - GFG/maximize-the-cut-segments.cpp
@@ -13,17 +13,19 @@ class Solution
     //Function to find the maximum number of cuts.
     int maximizeTheCuts(int n, int x, int y, int z)
     {
-        vector<int> dp(n+1);
+        vector<int> dp(n+1,-1);
+        
+        dp[0] = 0;
         
         for(int i=1;i<=n;i++){
             
-            if(i-x >= 0 && (dp[i-x] != 0 || i-x == 0))dp[i] = max(dp[i] , 1 + dp[i-x]);
-            if(i-y >= 0 && (dp[i-y] != 0 || i-y == 0))dp[i] = max(dp[i] , 1 + dp[i-y]);
-            if(i-z >= 0 && (dp[i-z] != 0 || i-z == 0))dp[i] = max(dp[i] , 1 + dp[i-z]);
+            if(i-x >= 0 && dp[i-x] != -1)dp[i] = max(dp[i] , 1 + dp[i-x]);
+            if(i-y >= 0 && dp[i-y] != -1)dp[i] = max(dp[i] , 1 + dp[i-y]);
+            if(i-z >= 0 && dp[i-z] != -1)dp[i] = max(dp[i] , 1 + dp[i-z]);
             
         }
         
-        return dp[n];
+        return max(0,dp[n]);
     }
 };
 
