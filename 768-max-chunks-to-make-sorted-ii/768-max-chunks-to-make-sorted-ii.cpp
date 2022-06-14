0c72087fd92b9bda933b3f1c0ab18c8923f4574c
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-06-14 18:43:18 +0530

    Time: 16 ms (48.96%), Space: 12.4 MB (59.58%) - LeetHub

--- 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.cpp
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> rmin(n+1);
+        int lmaxi=INT_MIN;
+        int count=0;
+         rmin[n]=INT_MAX;
         
-        int n = arr.size();
+          for(int i=n-1;i>=0;i--){
+            rmin[i]=min(rmin[i+1],arr[i]);
+          }
         
-        vector<int> lmax(n),rmin(n);
         
-        lmax[0] = arr[0];
-        for(int i=1;i<n;i++){
-            lmax[i] = max(lmax[i-1],arr[i]);
-        }
+          for(int i=0;i<n;i++){
+             
+                lmaxi=max(lmaxi,arr[i]);
+             
+                if(lmaxi<=rmin[i+1]) 
+                    count++;
+          }
         
-        rmin[n-1] = INT_MAX;
+//           for(int i=0;i<=n;i++){
+//               cout<<rmin[i]<<" ";
+//           }
+//           cout<<endl;
         
-        for(int i=n-2;i>=0;i--){
-            rmin[i] = min(arr[i+1],rmin[i+1]);
-        }
-        
-        int cnt = 0;
-        
-        for(int i=0;i<n;i++){
-            if(lmax[i] <= rmin[i])cnt++;
-        }
-        
-        return cnt;
+        return count;
     }
 };
