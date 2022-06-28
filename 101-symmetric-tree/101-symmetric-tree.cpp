909f64aa5181f26834f72cd57616e44a3cf71883
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-06-28 12:36:29 +0530

    Time: 6 ms (71.57%), Space: 16.9 MB (7.57%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -2,44 +2,35 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         
-        queue<TreeNode*> q;
+        if(!root->left && !root->right)return true;
         
-        q.push(root);
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
         
-        while(q.size()){
+        if(root->left)q1.push(root->left);
+        if(root->right)q2.push(root->right);
+        
+        if(q1.size() != q2.size() || q1.front()->val != q2.front()->val)return false;
+        
+        while(q1.size() && q2.size()){
             
-          int size = q.size();
-          vector<int> res;
+             TreeNode* node1 = q1.front();
+             TreeNode* node2 = q2.front();
             
-          for(int i=0;i<size;i++){
-              
-              TreeNode* node = q.front();
-              q.pop();
-              
-              if(node->left){
-                  q.push(node->left);
-                  res.push_back(node->left->val);
-              }
-              else res.push_back(-101);
-              
-              if(node->right){
-                  res.push_back(node->right->val);
-                  q.push(node->right);
-              }
-              else res.push_back(-101);
-              
-          }
-          int n = res.size() ;
+             q1.pop(),q2.pop();
             
-          if(n%2 != 0)return false;
+             if(!node1 && !node2)continue;
+             else if(!node1 || !node2)return false;
             
-          int i = 0 , j = n-1;
+             // cout<<node1->val<<" "<<node2->val<<endl;
+             if(node1->val != node2->val)return false;
+                 
+             q1.push(node1->left);
+             q1.push(node1->right);
             
-          while(i < j){
-              if(res[i] != res[j])return false;
-              i++,j--;
-          }
-            
+             q2.push(node2->right);
+             q2.push(node2->left);
+                 
         }
         
         return true;
