6ce3b14a9d6011fccd127860e09ef805767b227a
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-06-29 19:51:24 +0530

    Added solution - LeetHub

---
+++ Tree from Postorder and Inorder - GFG/tree-from-postorder-and-inorder.cpp
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+/* program to construct tree using inorder and postorder traversals */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node* node) {
+    if (node == NULL) return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
+
+Node* buildTree(int in[], int post[], int n);
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int in[n], post[n];
+        for (int i = 0; i < n; i++) cin >> in[i];
+        for (int i = 0; i < n; i++) cin >> post[i];
+        Node* root = buildTree(in, post, n);
+        preOrder(root);
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+        unordered_map<int,int> mp;
+    
+    Node* find(int instart,int inend,int in[],int poststart,int postend,int post[]){
+        
+        if(instart > inend || poststart > postend)return NULL;
+        
+        Node* root = new Node(post[postend]);
+        
+        if(postend == poststart){
+            return root;
+        }
+        
+        int loc = mp[root->data];
+        int ele = loc-instart;
+        
+        root->left = find(instart,loc-1,in,poststart,poststart + ele-1,post);
+        root->right = find(loc+1,inend,in,poststart+ele,postend-1,post);
+        
+        return root;
+    }
+    
+Node *buildTree(int in[], int post[], int n) {
+
+        for(int i=0;i<n;i++){
+            mp[in[i]] = i;
+        }
+        
+        return find(0,n-1,in,0,n-1,post);
+        
+   
+}
