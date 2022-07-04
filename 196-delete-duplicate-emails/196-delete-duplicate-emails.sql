165a56bed7e6e50adcd6ee7fa19fa427b5865b5b
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-07-04 18:08:32 +0530

    Time: 1430 ms (8.94%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -1,1 +1,2 @@
-delete from Person where id not in (select *  from (select min(id) from Person group by email) as T1);
+delete p1 from person p1,person p2
+where p1.email = p2.email and p1.id > p2.id;
