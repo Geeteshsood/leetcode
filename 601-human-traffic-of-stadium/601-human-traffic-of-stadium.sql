0f904bb1e2eb6e32ab1190abc3afa5d6fbb8a487
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-07-08 15:38:41 +0530

    Time: 271 ms (97.98%), Space: 0B (100.00%) - LeetHub

---
+++ 601-human-traffic-of-stadium/601-human-traffic-of-stadium.sql
@@ -0,0 +1,6 @@
+select distinct s1.id,s1.visit_date,s1.people from stadium s1,stadium s2,stadium s3 
+where 
+((s1.id = s2.id -1 and s1.id = s3.id - 2) or
+(s1.id = s2.id + 1 and s1.id = s3.id - 1) or
+(s1.id = s2.id + 2 and s1.id = s3.id + 1) )
+and s1.people >= 100 and s2.people >= 100 and s3.people >= 100 order by s1.visit_date;
