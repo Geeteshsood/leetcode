b640c2a4b67de677a6d21f9c5b532ea57216346f
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-07-12 20:03:17 +0530

    Time: 27 ms (84.67%), Space: 13 MB (95.30%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,39 +1,77 @@
 class Solution {
 public:
-    typedef pair<int,ListNode*> pi;
+    
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+       ListNode* temp = NULL , *node = NULL;  
+        
+       if(!l1 && !l2)return NULL;
+       else if(l1 && l2==NULL)return l1; 
+       else if(l1==NULL && l2)return l2;
+
+        
+       while(l1!=NULL && l2!=NULL){
+         
+          if(l1->val<l2->val){
+               if(temp == NULL){
+                 temp = l1;
+                 node = l1;
+                 l1 = l1->next;
+               }
+               else{
+                 temp->next = l1;
+                 temp = l1;
+                 l1 = l1->next;
+               }    
+          }
+          else{
+              if(temp == NULL){
+                 temp = l2;
+                 node = l2;
+                 l2 = l2->next;
+              }
+              else{
+                temp->next = l2;
+                temp = l2;
+                l2 = l2->next;
+              }    
+          }
+       }  
+        
+       while(l1){
+         temp->next = l1;
+         temp = l1;
+         l1 = l1->next;
+       }
+       
+       while(l2){
+         temp->next = l2;
+         temp = l2;
+         l2 = l2->next;
+       } 
+        
+     return node;   
+    }
+    
+    ListNode* merge(int start,int end,vector<ListNode*> &lists){
+        
+        if(start > end)return NULL;
+        else if(start == end){
+            return lists[start];
+        }
+        
+        int mid = start + (end - start)/2;
+        
+        ListNode* l1 = merge(start,mid,lists);
+        ListNode* l2 = merge(mid+1,end,lists);
+        
+        return mergeTwoLists(l1,l2);
+    }
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
         int k = lists.size();
-        ListNode* pre = NULL , *head = NULL;
+        return merge(0,k-1,lists);
         
-        priority_queue<pi,vector<pi>,greater<pi>> pq;
-
-        for(int i=0;i<k;i++){
-            if(lists[i]){
-                pq.push({lists[i]->val,lists[i]});
-            }
-        }
-        
-        while(pq.size()){
-            
-            ListNode* node = pq.top().second;
-            
-            pq.pop();
-            
-            if(node->next)pq.push({node->next->val,node->next});
-            
-            if(head == NULL){
-                head = node;
-                pre =  node;
-            }
-            else {
-                pre->next = node;
-                pre = pre->next;
-            }
-            
-        }
-        
-        return head;
     }
 };
