ccc79e8479357ee2d3f70c70c248c2d25508d4dc
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-07-16 22:12:10 +0530

    Time: 252 ms (95.45%), Space: 65.3 MB (84.27%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -6,93 +6,58 @@ public:
         Node* links[2];
     };
      
-    Node* root;
-    
-    void insert(string &s){
-        
-        Node* node = root;
-        
-        int n = s.size();
-        
-        for(int i=0;i<n;i++){
-            
-            int x = s[i]-'0';
-            
-            if(node->links[x] == NULL){
-                Node* temp = new Node();
-                node->links[x] = temp;
-            }
-            
-            node = node->links[x];
-        }
-        
-    }
-    
-    int findmaxy(string &s){
-        
-        Node* node = root;
-        int n = s.size() , val = 0;
-        
-        for(int i=0;i<n;i++){
-            
-            int x = !(s[i]-'0');
-            
-            if(node->links[x] == NULL){
-                x = !x;
-                
-                if(x)val += (1<<(30-i));
-                
-                node = node->links[x];
-            }
-            else{
-                
-                if(x)val += (1<<(30-i));
-                
-                node = node->links[x];
-            }
-        }
-        
-        return val;
-    }
-    
     int findMaximumXOR(vector<int>& nums) {
         
-        unordered_map<int,string> mp;
-        
-        root = new Node();
+        Node* root = new Node();
         
         int n = nums.size();
         
         for(int i=0;i<n;i++){
             
             int num = nums[i];
-            string str;
+            Node* node = root;
             
-            for(int j=30;j>=0;j--){
+            for(int j=31;j>=0;j--){
                 
-                  if(num >= (1<<j)){
-                      num -= (1<<j);
-                      str += '1';
-                  }
-                  else str += '0';
+               int bit = (num>>j) & 1;
                 
+               if(node->links[bit] == NULL){
+                   Node* temp = new Node();
+                   node->links[bit] = temp;
+               }
+                
+               node = node->links[bit];
             }
-            
-            mp[i] = str;
-            
-            insert(str);
         }
         
         int ans = INT_MIN;
         
         for(int i=0;i<n;i++){
             
-            int num = nums[i];
-
-            int val = findmaxy(mp[i]);
+            int num = nums[i] , val = 0;
+            Node* node = root;
+            
+            for(int j=31;j>=0;j--){
+                
+               int bit = (num>>j) & 1;
+                
+               if(node->links[!bit] == NULL){
+                  
+                   if(bit)val += (1<<j);
+                   
+                   node = node->links[bit];
+                   
+               }
+               else{ 
+                 if(!bit)val += (1<<j);
+                  
+                 node = node->links[!bit];
+               }
+            }
+            
             ans = max(ans,val^nums[i]);
         }
         
-        return ans;
+      return ans;
     }
 };
