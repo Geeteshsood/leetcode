c78a45e809c21cc865f2a34f1fcf1324109deaee
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-07-16 21:07:49 +0530

    Time: 701 ms (26.49%), Space: 171.8 MB (8.60%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,107 @@
+class Solution {
+public:
+    
+    class Node{
+        public:
+        Node* links[2];
+    };
+     
+    Node* root;
+    
+    void insert(string &s){
+        
+        Node* node = root;
+        
+        int n = s.size();
+        
+        for(int i=0;i<n;i++){
+            
+            int x = s[i]-'0';
+            
+            if(node->links[x] == NULL){
+                Node* temp = new Node();
+                node->links[x] = temp;
+            }
+            
+            node = node->links[x];
+        }
+        
+    }
+    
+    int findmaxy(string &s){
+        
+        Node* node = root;
+        int n = s.size() , val = 0;
+        
+        for(int i=0;i<n;i++){
+            
+            int x = s[i]-'0';
+            
+            if(node->links[x] == NULL){
+                x = !x;
+                
+                if(x)val += (1<<(30-i));
+                
+                node = node->links[x];
+            }
+            else{
+                
+                if(x)val += (1<<(30-i));
+                
+                node = node->links[x];
+            }
+        }
+        
+        return val;
+    }
+    
+    int findMaximumXOR(vector<int>& nums) {
+        
+        unordered_map<int,string> mp;
+        
+        root = new Node();
+        
+        int n = nums.size();
+        
+        for(int i=0;i<n;i++){
+            
+            int num = nums[i];
+            string str;
+            
+            for(int j=30;j>=0;j--){
+                
+                  if(num >= (1<<j)){
+                      num -= (1<<j);
+                      str += '1';
+                  }
+                  else str += '0';
+                
+            }
+            
+            insert(str);
+        }
+        
+        int ans = INT_MIN;
+        
+        for(int i=0;i<n;i++){
+            
+            int num = nums[i];
+            string str;
+            
+            for(int j=30;j>=0;j--){
+                
+                  if(num >= (1<<j)){
+                      num -= (1<<j);
+                      str += '0';
+                  }
+                  else str += '1';
+                
+            }
+            
+            int val = findmaxy(str);
+            ans = max(ans,val^nums[i]);
+        }
+        
+        return ans;
+    }
+};
