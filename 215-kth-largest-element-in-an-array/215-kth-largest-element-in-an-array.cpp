9ee75ac7a32ce3695e34c788c2b633fd1cd7459b
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-02 10:12:03 +0530

    Time: 306 ms (5.04%), Space: 45.5 MB (10.79%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+   int quickselect(int start , int end,vector<int> &nums,int k){
+        
+        int n = nums.size();
+
+        int i = start , j = start;
+       
+        while(i <= end){
+            
+            if(nums[i] <= nums[end]){
+                swap(nums[i],nums[j]);
+                i++,j++;
+            }
+            else if(nums[i] > nums[end]){
+                 i++;
+            }
+        }
+        
+        int idx = j-1;
+       
+        if(idx > k){
+            return quickselect(start,idx-1,nums,k);
+        }
+        else if(idx < k){
+            return quickselect(idx+1,end,nums,k);
+        }
+       
+         return nums[idx];
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        k =  n - k;
+        
+        return quickselect(0,n-1,nums,k);
+        
+    }
+};
