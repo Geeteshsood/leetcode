ad4190b68b3bdd981061614a2fa611cfc8fc1df0
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-05 22:48:16 +0530

    Time: 6 ms (58.15%), Space: 11.1 MB (29.09%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
-    int binarysearch(vector<int>&nums,int low,int high,int target)
-    {
-        
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            
-            if(nums[mid]==target)
-                return mid;
-            
-            else if(nums[mid]<target)
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
-        }
-        return -1;
-    }
-    
     int search(vector<int>& nums, int target) {
         
-        int i=0;
+        int start = 0 , end = nums.size()-1;
         
-        while(i < nums.size()-1 && nums[i]<nums[i+1])
-        {
-            i++;
+        while(start <= end){
+            
+           int mid = start + (end - start)/2;
+            
+         
+           if(nums[mid] == target){
+               return mid;
+           }
+           else if(nums[mid] >= nums[start]){
+               // first half
+               
+               if(target >= nums[start]){
+                   if(target > nums[mid])start = mid+1;
+                   else end = mid-1;
+               }
+               else {
+                    start = mid+1;
+               }
+           }
+           else{
+               // second half
+               
+               if(target < nums[start]){
+                   if(target > nums[mid])start = mid+1;
+                   else end = mid-1;
+               }
+               else {
+                    end = mid-1;
+               }
+           }
+         
         }
         
-        i=i+1;
-        
-        int count1=binarysearch(nums,i,nums.size()-1,target);
-        
-        int count2=binarysearch(nums,0,i-1,target);
-        
-        if(count1!=-1)
-        {
-            return count1;
-        }
-        else
-            return count2;
+        return -1;
     }
 };
