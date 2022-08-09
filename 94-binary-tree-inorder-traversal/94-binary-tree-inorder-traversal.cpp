9adc9caac87fad0db69cb1a9d12b0627ee9cbc1d
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-10 05:16:02 +0530

    Time: 3 ms (60.87%), Space: 8.4 MB (74.76%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,42 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         
-        stack<TreeNode*> st;
-        vector<int> res;
+        vector<int> ans;
+        TreeNode* cur = root;
         
-        while(root){
-            st.push(root);
-            root = root->left;
+        while(cur){
+            
+            if(cur->left == NULL){
+                ans.push_back(cur->val);
+                cur = cur->right;
+            }
+            else{
+                
+               TreeNode* pre = cur->left;
+                   
+               while(pre->right != NULL && pre->right != cur){
+                   pre = pre->right;
+               }
+               
+               if(pre->right == NULL){
+                   pre->right = cur;
+                   cur = cur->left;
+               }
+               else{
+                   
+                   pre->right = NULL;
+                   ans.push_back(cur->val);
+                   cur = cur->right;
+                   
+               }
+                   
+            }
         }
         
-        while(st.size()){
-            
-          TreeNode* node = st.top();
-          st.pop();
-            
-          res.push_back(node->val);
-          
-          node = node->right;
-            
-          while(node){
-              st.push(node);
-              node = node->left;
-          }
-            
-        }
-        
-        return res;
+     return ans;
     }
 };
