64c501b12ffbac68acb81f7a5060dccb4fdb121a
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-09 11:57:49 +0530

    Time: 9 ms (5.30%), Space: 8.4 MB (76.59%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -1,40 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        
+              
+        stack<TreeNode*> st;
         vector<int> res;
+        if(root)st.push(root);
         
-        TreeNode *cur = root;
+        while(st.size()){
             
-        while(cur != NULL){
+          TreeNode* node = st.top();
+          st.pop();
             
-            if(cur->left == NULL){
-                
-                res.push_back(cur->val);
-                cur = cur->right;
-                
-            }
-            else{
-                
-                TreeNode* prev = cur->left;
-                
-                while(prev->right != NULL && prev->right != cur){
-                    prev = prev->right;
-                }
-                
-                if(prev->right == NULL){
-                    res.push_back(cur->val);
-                    prev->right = cur;
-                    cur = cur->left;
-                }
-                else{
-                    prev->right = NULL;
-                    cur = cur->right;
-                }
-            }
+          res.push_back(node->val);
+          if(node->right)st.push(node->right);
+            
+          while(node->left){
+              node = node->left;
+              if(node->right)st.push(node->right);
+              res.push_back(node->val);
+          }
             
         }
         
-       return res; 
+        return res;
+        
     }
 };
