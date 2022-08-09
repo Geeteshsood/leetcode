f6c0004e65c4f851b70e2b750d6d53f81088ae77
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-09 12:36:32 +0530

    Time: 4 ms (41.91%), Space: 8.5 MB (63.74%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -1,22 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-     void dfs(TreeNode* root,vector<int> &res){
-        
-        if(root == NULL)return ;
-        
-        dfs(root->left,res);
-        dfs(root->right,res);
-        
-        res.push_back(root->val); 
-         
-    }
-    
     vector<int> postorderTraversal(TreeNode* root) {
         
-        vector<int> res;
-        dfs(root,res);
+        stack<TreeNode*> st1,st2;
         
-       return res; 
+        if(root)st1.push(root);
+        
+        while(st1.size()){
+            
+            TreeNode* temp = st1.top();
+            st1.pop();
+            
+            st2.push(temp);
+            
+            TreeNode* node = st2.top();
+            
+            if(node->left)st1.push(node->left);
+            if(node->right)st1.push(node->right);
+            
+        }
+        
+        vector<int> res;
+        
+        while(st2.size()){
+            
+           int val = st2.top()->val;
+           st2.pop();
+            
+           res.push_back(val);
+            
+        }
+        
+    return res;
     }
 };
