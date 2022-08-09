18a6e15f27c74ffe10b3ed96706b813929a3cbaa
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-09 12:00:47 +0530

    Time: 3 ms (60.87%), Space: 8.3 MB (74.76%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,38 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-       
-        vector<int> inorder;
         
-        TreeNode* cur = root;
+        stack<TreeNode*> st;
+        vector<int> res;
         
-        while(cur){
+        while(root){
+            st.push(root);
+            root = root->left;
+        }
+        
+        while(st.size()){
             
-          if(cur->left == NULL){
-              inorder.push_back(cur->val);
-              cur = cur->right;
+          TreeNode* node = st.top();
+          st.pop();
+            
+          res.push_back(node->val);
+          
+          node = node->right;
+            
+          while(node){
+              st.push(node);
+              node = node->left;
           }
-          else{
-              
-              TreeNode* pre = cur->left;
-              
-              while(pre->right != NULL && pre->right != cur){
-                  pre = pre->right;
-              }
-
-              if(pre->right == NULL){
-                  pre->right = cur;
-                  cur = cur->left;
-              }
-              else{
-                  pre->right = NULL;
-                  inorder.push_back(cur->val);
-                  cur = cur->right;
-              }
-          }  
             
         }
         
-     return inorder;
+        return res;
     }
 };
