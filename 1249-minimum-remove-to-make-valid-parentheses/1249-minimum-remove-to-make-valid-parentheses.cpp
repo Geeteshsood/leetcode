dacaf64aac77a7dd614221aff087c4a92043eb22
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-10 08:56:26 +0530

    Time: 26 ms (79.52%), Space: 10.8 MB (80.34%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,25 +2,31 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         
-         int n=s.length();
-         vector<pair<char,int>> res;   
-         for(int i=0;i<n;i++){
-                 
-           if(s[i]=='(')res.push_back({s[i],i});
-           else if(s[i]==')'){   
-                if(res.empty() || res.back().first==')')res.push_back({s[i],i});
-                else res.pop_back();
-           }
-         } 
-       for(int i=n-1;i>=0;i--){
-           if(!res.empty() && i==res.back().second){
-                  s[i]=' ';
-                  res.pop_back();
-           } 
-       }  
-       
-       s.erase(remove(s.begin(),s.end(),' '),s.end());     
-            
-return s;            
+        int n=s.size();
+
+        stack<int> a;
+      unordered_map<char,char>m;
+        m['(']=')';
+        
+        for(int i=0;i<n;i++){
+            char c=s[i];
+            if(c=='(')a.push(i);
+            else if(c==')'){
+                if(a.size()>0 && m[s[a.top()]]==c)a.pop();
+                else a.push(i);
+            }
+        }
+        
+        if(a.empty())return s;
+        
+      while(!a.empty()){
+          int x = a.top();
+          s[x] = ' ';
+          a.pop();
+      }
+        
+      s.erase(remove(s.begin(),s.end(),' '),s.end()); 
+        
+    return s;
     }
 };
