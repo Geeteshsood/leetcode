ec824130df1cbdc3a3a2ef9262da7f0b9f1543b8
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-10 07:20:09 +0530

    Time: 19 ms (89.11%), Space: 26.3 MB (43.41%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,35 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-        
-    unordered_map<int,int> mp;
     
-    TreeNode* find(int instart,int inend,vector<int> &in,int prestart,int preend,vector<int> &pre){
+      unordered_map<int,int> mp;
+    
+TreeNode* find(int instart,int inend,vector<int> &in,int prestart,int preend,vector<int> &pre){
         
         if(instart > inend || prestart > preend)return NULL;
         
-        TreeNode* root = new TreeNode(pre[prestart]);
+        int val = pre[prestart];
         
-        if(preend == prestart){
-            return root;
-        }
+        TreeNode* root = new TreeNode(val);
         
-        int loc = mp[root->val];
-        int ele = loc-instart;
+        int idx = mp[val];
+        int len = idx - instart;
         
-        root->left = find(instart,loc-1,in,prestart+1,prestart + ele,pre);
-        root->right = find(loc+1,inend,in,prestart+ele+1,preend,pre);
+        root->left = find(instart,idx-1,in,prestart+1,prestart + len,pre);
+        root->right = find(idx+1,inend,in,prestart + len+1,preend,pre);
         
         return root;
     }
     
     TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
         
-        int n = in.size();
+         int n = pre.size();
         
         for(int i=0;i<n;i++){
             mp[in[i]] = i;
         }
         
-        return find(0,n-1,in,0,n-1,pre);
+        TreeNode* root = find(0,n-1,in,0,n-1,pre);
+        
+        return root;
     }
 };
