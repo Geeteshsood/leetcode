a0b6de8a5a86d6b90dafb1669a0f080eb60dead0
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-10 08:57:00 +0530

    Time: 90 ms (6.49%), Space: 12.2 MB (23.85%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    string minRemoveToMakeValid(string s) {
+   string minRemoveToMakeValid(string s) {
+        string d="";
         
         int n=s.size();
-
-        stack<int> a;
+       for( int i=0;i<n;i++){
+           d+=s[i];
+       }
+        stack<int>a;
       unordered_map<char,char>m;
         m['(']=')';
-        
         for(int i=0;i<n;i++){
             char c=s[i];
             if(c=='(')a.push(i);
             else if(c==')'){
                 if(a.size()>0 && m[s[a.top()]]==c)a.pop();
-                else a.push(i);
+         else a.push(i);
             }
         }
-        
         if(a.empty())return s;
-        
       while(!a.empty()){
-          int x = a.top();
-          s[x] = ' ';
+          d.erase(d.begin()+a.top());
           a.pop();
       }
-        
-      s.erase(remove(s.begin(),s.end(),' '),s.end()); 
-        
-    return s;
+        return d;
     }
 };
