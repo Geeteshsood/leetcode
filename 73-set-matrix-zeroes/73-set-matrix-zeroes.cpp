8390809b87e28106efe549a58927a227bf192e6b
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-11 12:08:10 +0530

    Time: 23 ms (53.11%), Space: 13.2 MB (81.84%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -4,25 +4,54 @@ public:
         
         int r = mat.size() , c = mat[0].size();
     
-        vector<int> row(r,-1) , col(c,-1);
+        bool isCol = false;
         
         for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+            
+            if(mat[i][0] == 0){
+                isCol = true;
+            }
+            
+            for(int j=1;j<c;j++){
+                
                 if(mat[i][j] == 0){
-                    row[i] = 0;
-                    col[j] = 0;
+                  mat[i][0] = 0;
+                  mat[0][j] = 0;
+                }
+            }
+            
+        }
+        
+        // for(int i=0;i<r;i++){
+        //     for(int j=0;j<c;j++){
+        //         cout<<mat[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        
+        for(int i=1;i<r;i++){
+
+            for(int j=1;j<c;j++){
+                if(mat[i][0] == 0|| mat[0][j] == 0){
+                    mat[i][j] = 0;
                 }
             }
         }
+
+        for(int j=0;j<c;j++){
+            if(mat[0][0] == 0){
+                mat[0][j] = 0;
+            }
+        }
         
         for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-               if(row[i] == 0 || col[j] == 0){
-                   mat[i][j] = 0;
-               } 
+            if(isCol){
+                mat[i][0] = 0;
             }
         }
         
         
+     
     }
 };
