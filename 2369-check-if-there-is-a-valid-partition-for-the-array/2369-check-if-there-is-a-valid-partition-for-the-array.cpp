fe8a4d64109e35f4f69fd41288f3cf422d00b696
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-14 16:00:51 +0530

    Time: 319 ms (11.11%), Space: 83.9 MB (22.22%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -4,25 +4,25 @@ public:
         
         int n = nums.size();
         
-        vector<int> dp(n+1);
+        vector<int> dp(4);
         dp[0] = true;
         
         for(int i=1;i<n;i++){
    
             if(nums[i] == nums[i-1]){
-                dp[i+1] = dp[i+1] | dp[i-1];
+                dp[(i+1)%4] = dp[(i-1)%4];
                 
-                if(i > 1 && nums[i] == nums[i-2]){
-                    dp[i+1] = dp[i+1] | dp[i-2];
+                if(i > 1 && nums[i] == nums[i-2] && !dp[(i+1)%4]){
+                    dp[(i+1)%4] = dp[(i-2)%4];
                 }
             }
             else if(i > 1 && ((nums[i] == nums[i-1]+1 ) && (nums[i] == nums[i-2] + 2))){
-                 dp[i+1] = dp[i+1] | dp[i-2];
+                 dp[(i+1)%4] = dp[(i-2)%4];
             }
-            
+            else dp[(i+1)%4] = false;
             
         }
         
-        return dp[n];
+        return dp[n%4];
     }
 };
