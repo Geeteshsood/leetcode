d25500a71184b1a3014071ccbcd122a67f600e5d
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-14 14:29:32 +0530

    Time: 248 ms (90.38%), Space: 102.9 MB (70.04%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -3,50 +3,45 @@ public:
     
     int ans = INT_MIN;
     
-void dfs(int i,vector<bool> &vis,vector<bool> &path,vector<int> &order,vector<vector<int>> &adj){
+void dfs(int i,vector<bool> &vis,vector<bool> &path,vector<int> &order,vector<int> &edges){
         
         vis[i] = true;
         path[i] = true;
             
-        for(auto &it : adj[i]){
-            
-           if(!vis[it]){
-               
-               int x = order[it];
-               order[it] = order[i] + 1;
-               
-               dfs(it,vis,path,order,adj);
-               
-               order[it] = x;
-           } 
-           else if(path[it]){
-               int len = order[i] - order[it] + 1;
-               ans = max(ans,len);
-               return ;
-           }
-            
+        int it = edges[i];
+    
+        if(it == -1){
+            path[i] = false;
+            return ;
         }
-        
+    
+        if(!vis[it]){
+            
+            int x = order[it];
+            order[it] = order[i] + 1;
+            
+            dfs(it,vis,path,order,edges);
+            
+            order[it] = x;
+        } 
+        else if(path[it]){
+            int len = order[i] - order[it] + 1;
+            ans = max(ans,len);
+            return ;
+        }
+            
          path[i] = false;
-        
     }
     
     int longestCycle(vector<int>& edges) {
         
         int n = edges.size();
-        
-        vector<vector<int>> adj(n);
-        
-        for(int i=0;i<n;i++){
-            if(edges[i] != -1)
-            adj[i].push_back(edges[i]);
-        }
-        
+
         vector<bool> vis(n),path(n);
         vector<int> order(n,1);
         
         for(int i=0;i<n;i++){
-            if(!vis[i])dfs(i,vis,path,order,adj);
+            if(!vis[i])dfs(i,vis,path,order,edges);
         }
         
         if(ans == INT_MIN)return -1;
