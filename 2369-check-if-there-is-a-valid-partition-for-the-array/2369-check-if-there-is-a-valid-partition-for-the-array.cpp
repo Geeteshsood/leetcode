d0b3d2be8328b5b2b84207483778ff6e2b2ce7b6
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-14 15:53:21 +0530

    Time: 427 ms (11.11%), Space: 87.4 MB (22.22%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -4,26 +4,25 @@ public:
         
         int n = nums.size();
         
-        unordered_map<int,int> dp;
-        dp[-1] = true;
+        vector<int> dp(n+1);
+        dp[0] = true;
         
         for(int i=1;i<n;i++){
-            
-            
+   
             if(nums[i] == nums[i-1]){
-                dp[i] = dp[i] | dp[i-2];
+                dp[i+1] = dp[i+1] | dp[i-1];
                 
                 if(i > 1 && nums[i] == nums[i-2]){
-                    dp[i] = dp[i] | dp[i-3];
+                    dp[i+1] = dp[i+1] | dp[i-2];
                 }
             }
             else if(i > 1 && ((nums[i] == nums[i-1]+1 ) && (nums[i] == nums[i-2] + 2))){
-                 dp[i] = dp[i] | dp[i-3];
+                 dp[i+1] = dp[i+1] | dp[i-2];
             }
             
             
         }
         
-        return dp[n-1];
+        return dp[n];
     }
 };
