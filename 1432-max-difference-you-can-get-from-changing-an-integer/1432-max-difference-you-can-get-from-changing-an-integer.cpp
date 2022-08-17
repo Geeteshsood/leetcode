8b4d4e748065adbd596f5d65201d7a344ac885bd
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-17 20:24:57 +0530

    Time: 5 ms (22.35%), Space: 6 MB (28.49%) - LeetHub

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -3,71 +3,45 @@ public:
     int maxDiff(int num) {
         
         string s = to_string(num);
+        string str = s;
+        
         int n = s.size();
         
-        vector<int> vis(10);
-        for(int i=0;i<n;i++){
-            vis[s[i]-'0'] = true;
+        int i = 0;
+        while(i < n && s[i] == '9'){
+            i++;
         }
         
-        int ans = INT_MIN;
+        int ch = s[i]-'0';
         
-        for(int x=0;x<10;x++){
-            
-            if(!vis[x])continue;
-            
-            for(int y=0;y<10;y++){
-                
-                string str = s;
-                
-                for(int i=0;i<n;i++){
-                    
-                    int ch = str[i]-'0';
-                    
-                    if(ch == x){
-                        if(i == 0 && (y == 0))break;
-                        str[i] = (y + '0');
-                    }
-                }
-                
-                int a = stoi(str);
-                // if(x == 1 || x == 4)cout<<a<<endl;
-                
-                for(int u=0;u<10;u++){
-                    
-                  if(!vis[u])continue;
-                    
-                  for(int v=0;v<10;v++){
-                      
-                      string res = s;
-                      
-                      for(int i=0;i<n;i++){
-                          
-                          int ch = res[i]-'0';
-                          
-                          if(ch == u){
-                              if(i == 0 && (v == 0))break;
-                              res[i] = (v + '0');
-                          }
-                      }
-                      
-                      // cout<<str<<" "<<res<<endl;
-                      
-                      int b = stoi(res);
-                      int val = abs(a-b);
-                      
-                      if(a == 0 || b == 0)continue;
-                      
-                      if(val > ans){
-                          // if(val == 899)cout<<a<<" "<<b<<endl;
-                          ans = val;
-                      }
-                  }
-                }
-                
+        for(int i=0;i<n;i++){
+            if(s[i]-'0' == ch){
+                s[i] = 9 + '0';
             }
         }
         
-        return ans;
+        i = 0;
+        while(i < n && str[i]-'0' <= 1){
+            i++;
+        }
+        
+        ch = str[i]-'0';
+        
+        int x = 1;
+        
+        if(i < n-1 && i != 0)x = 0;
+       
+           ch = str[i]-'0';
+         
+           for(;i<n;i++){
+               if(str[i]-'0' == ch){
+                   str[i] = x + '0';
+               }
+           }
+        
+        int a = stoi(s);
+        int b = stoi(str);
+        
+        return abs(b-a);
     }
 };
