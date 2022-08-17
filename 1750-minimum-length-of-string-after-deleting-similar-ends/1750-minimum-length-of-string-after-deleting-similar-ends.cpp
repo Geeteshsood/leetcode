6aac547168dd53cf4b319b02caeccf45d5092f98
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-17 20:07:59 +0530

    Time: 36 ms (60.86%), Space: 12.8 MB (15.05%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int minimumLength(string s) {
+    
+    int minimumLength(string s){
         
-        int n = s.size();
+        int n=s.size();
         
-        int i = 0 , j = n-1;
+        int i=0;
+        int j=n-1;
         
-        while(i < j && s[i] == s[j]){
-            
-            while(i < j && s[i] == s[i+1]){
-                i++;
-            }
-            i++;
-            
-            while(j > i && s[j] == s[j-1]){
-                j--;
-            }
-            j--;
-            
-            // cout<<i<<" "<<j<<endl;
+        while(i<j && s[i]==s[j]){
+            char c=s[i];
+            while(i<j && s[i]==c)i++;
+            while(i<=j && s[j]==c)j--;
         }
+       
+        // cout<<i<<" "<<j<<endl;
+       
+        return j-i+1;
         
-        
-        return max(0,j-i+1);
     }
 };
