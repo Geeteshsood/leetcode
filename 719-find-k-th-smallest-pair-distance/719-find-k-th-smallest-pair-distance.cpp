02b6833712c69b3c86e0f8c0a4b403fa84e40567
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-18 09:20:58 +0530

    Time: 82 ms (12.16%), Space: 23.3 MB (5.74%) - LeetHub

--- 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     
-    int count(int mid,vector<int> &nums){
+    int count(int mid,vector<int> &nums,vector<int> &prefix){
         
-        int n = nums.size(),cnt = 0;
+        int n = nums.size(),cnt = 0,y=-1;
         
         for(int i=0;i<n;i++){
             
-             int low = nums[i] - mid;
             int high = nums[i] + mid;
             
-            int y = upper_bound(nums.begin(),nums.end(),high) - nums.begin();
+            if(high > nums[n-1]){
+                y = n;
+            }
+            else y = prefix[high];
             
             cnt += (y-i-1);
             
         }
-        
         
       return cnt;  
     }
@@ -23,8 +24,31 @@ public:
     int smallestDistancePair(vector<int>& nums, int k) {
         
         int n = nums.size();
-        
         sort(nums.begin(),nums.end());
+        // for(auto &it : nums){
+        //     cout<<it<<" ";
+        // }
+        // cout<<endl;
+        
+        vector<int> prefix(nums[n-1]+1);
+
+        int i = 0;
+        
+        for(int val=nums[0];val<=nums[n-1];val++){
+            int cnt = 0;
+            if(i == n || val != nums[i]){
+                if(val == 0)prefix[val] = 0;
+                else prefix[val] = prefix[val-1];
+            }
+            else{
+                while(i < n && nums[i]==val){
+                    cnt++;
+                    i++;
+                }
+                if(val == 0)prefix[val] = cnt;
+                else prefix[val] = prefix[val-1] + cnt;
+            }
+        }
         
         int start = 0;
         int end = nums[n-1]-nums[0];
@@ -34,7 +58,7 @@ public:
             
            int mid = start + (end - start)/2;
             
-           int cnt = count(mid,nums);
+           int cnt = count(mid,nums,prefix);
             
             // cout<<mid<<" "<<cnt<<endl;
             
