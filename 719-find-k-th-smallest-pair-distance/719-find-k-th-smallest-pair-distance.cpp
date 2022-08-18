c95003926afd9db3dbb6107930340746dd472bf3
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-18 08:37:45 +0530

    Time: 111 ms (7.85%), Space: 10.2 MB (41.01%) - LeetHub

---
+++ 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    
+    int count(int mid,vector<int> &nums){
+        
+        int n = nums.size(),cnt = 0;
+        
+        for(int i=0;i<n;i++){
+            
+             int low = nums[i] - mid;
+            int high = nums[i] + mid;
+            
+            int x = lower_bound(nums.begin(),nums.end(),low) - nums.begin();
+            int y = upper_bound(nums.begin(),nums.end(),high) - nums.begin();
+            
+            // cout<<nums[i]<<" , "<<low<<" , "<<high<<" -> "<<x<<" "<<y<<endl;
+            
+            cnt += (y-x-1);
+            
+        }
+        
+        
+      return cnt/2;  
+    }
+    
+    int smallestDistancePair(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        
+        sort(nums.begin(),nums.end());
+        
+        int start = 0;
+        int end = nums[n-1]-nums[0];
+        int ans = -1;
+        
+        while(start <= end){
+            
+           int mid = start + (end - start)/2;
+            
+           int cnt = count(mid,nums);
+            
+            // cout<<mid<<" "<<cnt<<endl;
+            
+           if(cnt >= k){
+               ans = mid;
+               end = mid-1;
+           }
+           else start = mid+1;
+            
+        }
+        
+        return ans;
+    }
+};
