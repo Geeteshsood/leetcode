522003d31c199d258f53897b3c7b67fb9f80013c
Geeteshsood <76897516+Geeteshsood@users.noreply.github.com>
2022-08-25 09:24:21 +0530

    Time: 7 ms (98.98%), Space: 8.7 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         
-        vector<int> freq(26);
+        vector<int> vis(26);
         
-        for(auto &ch : ransomNote){
-              freq[ch-'a']++;
-        }
-        for(auto &ch : magazine){
-              freq[ch-'a']--;
+        for(int i=0;i<magazine.size();i++){
+            vis[magazine[i]-'a']++;
         }
         
-        for(int i=0;i<26;i++){
-            if(freq[i] > 0)return false;
+        for(int i=0;i<ransomNote.size();i++){
+            vis[ransomNote[i]-'a']--;
+            
+            if(vis[ransomNote[i]-'a'] < 0)return false;
         }
         
-      return true;  
+        return true;
     }
 };
